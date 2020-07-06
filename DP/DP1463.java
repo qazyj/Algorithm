62a3c89f2639149192949ac360590babe8326e05
kimyoungJin12 <48886941+kimyoungJin12@users.noreply.github.com>
2020-07-06 19:17:22 +0900

    Update DP1463.java

--- DP/DP1463.java
+++ DP/DP1463.java
@@ -1,30 +1,21 @@
 import java.util.Scanner;
-import java.util.ArrayList;
-import java.util.List;
 
-public class Algorithm {
+public class Main {
     public static void main(String args[]) {
     	Scanner scanner = new Scanner(System.in);
         
-    	int n = scanner.nextInt(); 
-    	int k = scanner.nextInt();
-    	int m = scanner.nextInt()-1;
-    	ArrayList array = new ArrayList();
-    	for(int i=1;i<n+1;i++){
-    		array.add(i);	
-    	}
-    	while(array.size()!=1) {
-    		System.out.println(m+" "+array.size());
-    		System.out.println(array.get(m));
-    		array.remove(m);
-    		m = m+k/array.size();
-    	}
-    	System.out.println(array.get(0));
+    	int X = scanner.nextInt();     //정수 값 받아옴
+    	int[] array = new int[X+1];	    //받아온 정수만큼 크기의 배열 생성
     	
-    	//array.remove(3);
-    	//for(int i=0;i<array.size();i++)
-    	//	System.out.println(array.get(i));
-    	//System.out.println(array[X]);
+    	for(int i = 2; i<=X;i++) {
+    		if (i%3==0) 
+    			array[i] = Math.min(array[i-1], array[i/3])+1;
+    		else if (i%2==0) 
+    			array[i] = Math.min(array[i-1],  array[i/2])+1;
+    		else
+    			array[i] = array[i-1] + 1;
+    	}
+    	System.out.println(array[X]);
 		
 	}
 }
