ebf22d03f25f94a3e2a2a09668374b3e0d3bc41c
kimyoungjin12 <qazyj@naver.com>
2020-08-25 22:52:21 +0900

    boolean 배열에 초기 false로 채워두고 지나간 값은 true로 변환해준다. true일시 건너띄고 false일 시 true로 바꿔주면서 count++해준다. count가 K랑 같아질 시 출력하고 종료.

--- 구현/구현 2960(에라스토테네스의 체) - SILVER 4.java
+++ 구현/구현 2960(에라스토테네스의 체) - SILVER 4.java
@@ -4,7 +4,7 @@ import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
-public class Algorithm {
+public class Main {
 	
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub        
