474e3951b2e45b6af35c7eba78f2db21e86d509a
kimyoungjin12 <qazyj@naver.com>
2020-08-28 19:36:08 +0900

    이분 탐색을 통해 시간 복잡도 O(NlogN)로 풀었다.

---
+++ 이분 탐색/이분탐색 9702 (가장 긴 증가하는 부분 수열) - GOLD 2.java
@@ -0,0 +1,44 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Algorithm {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		long N = Integer.parseInt(br.readLine());
+		long[] inputArray = new long[(int) N];
+		long[] LIS = new long[(int) N];
+
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for (long i = 0; i < N; i++) {
+			inputArray[(int) i] = Integer.parseInt(st.nextToken());
+		}
+
+		LIS[0] = inputArray[0];
+		long j = 0;
+
+		for (long i = 1; i < N; i++) {
+			if (LIS[(int) j] < inputArray[(int) i]) {
+				LIS[(int) ++j] = inputArray[(int) i];
+			} else {
+				long ans = binarySearch(LIS, 0, j, inputArray[(int) i]);
+				LIS[(int) ans] = Math.min(LIS[(int) ans], inputArray[(int) i]);
+			}
+		}
+		System.out.println(j + 1);
+	}
+
+	static long binarySearch(long[] inputArray, int startIndex, long endIndex, long key) {
+		while (startIndex <= endIndex) {
+			int mid = (int) ((startIndex + endIndex) / 2); 
+			if (inputArray[mid] < key) {
+				startIndex = mid + 1;
+			} else {
+				endIndex = mid - 1;
+			}
+		}
+		return startIndex;
+	}
+}
