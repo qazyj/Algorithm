64997e641d2566399846db1029f1f2c401ea1bc9
kimyoungjin12 <qazyj@naver.com>
2020-09-01 19:13:52 +0900

    기존 N과 M(2)의 오름차순만 출력하는 부분, N과 M(3)의 중복된 수 허용을 합쳤다.

---
+++ 백트래킹/백트래킹 15652 (N과 M (4)) - SILVER 3.java
@@ -0,0 +1,55 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Algorithm {
+	private static int list[];
+	private static int n, m;
+	private static StringBuilder sb;
+
+	private static void dfs(int cnt) {
+		if (cnt == m) {
+			boolean check = true;
+			for (int i = 1; i < m; i++) {
+				if(list[i-1]>list[i])
+					check = false;
+			}
+			if(check) {
+				for (int i = 0; i < m; i++) {
+					sb.append(list[i]).append(" ");
+				}
+				sb.append("\n");
+			}
+			return;
+		}
+
+		for (int i = 1; i <= n; i++) {
+			list[cnt] = i;
+			dfs(cnt + 1);
+		}
+	}
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
+
+		list = new int[9];
+
+		sb = new StringBuilder();
+
+		dfs(0);
+
+		bw.write(sb.toString());
+
+		bw.close();
+		br.close();
+	}
+
+}
