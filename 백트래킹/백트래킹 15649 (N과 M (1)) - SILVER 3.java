7b6c52cbcd5599cea9409c74f6bfffd03bed4c5d
kimyoungjin12 <qazyj@naver.com>
2020-09-01 18:00:09 +0900

    백트래킹으로 품. 풀었다기보단 코드를 보면서 숙지를 했다.

---
+++ 백트래킹/백트래킹 15649 (N과 M (1)) - SILVER 3.java
@@ -0,0 +1,54 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Algorithm {
+	private static int list[];
+	private static boolean visit[];
+	private static int n, m;
+	private static StringBuilder sb;
+
+	private static void dfs(int cnt) {
+		if (cnt == m) {
+			for (int i = 0; i < m; i++) {
+				sb.append(list[i]).append(" ");
+			}
+			sb.append("\n");
+			return;
+		}
+
+		for (int i = 1; i <= n; i++) {
+			if (visit[i])
+				continue;
+			visit[i] = true;
+			list[cnt] = i;
+			dfs(cnt + 1);
+			visit[i] = false;
+		}
+	}
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
+
+		list = new int[9];
+		visit = new boolean[9];
+
+		sb = new StringBuilder();
+
+		dfs(0);
+
+		bw.write(sb.toString());
+
+		bw.close();
+		br.close();
+	}
+
+}
