e4c6bb468561f956dd6f8336f60da9ec01afe658
kimyoungjin12 <qazyj@naver.com>
2020-09-02 17:22:20 +0900

    기존 N과 M (9)의 코드에서 추가하는 부분에 boolean을 하나 추가해서 오름차순만 출력할 수 있게 하였다.

---
+++ 백트래킹/백트래킹 15664 (N과 M (10)) - SILVER 2.java
@@ -0,0 +1,70 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Algorithm {
+	private static int list[];
+	private static int listvalue[];
+	private static boolean visit[];
+	private static int n, m;
+	private static StringBuilder sb;
+
+	private static void dfs(int cnt) {
+		if (cnt == m) {
+			boolean check = true;
+			for (int i = 1; i < m; i++) {
+				if(list[i-1]>list[i])
+					check = false;
+			}
+			if(check) {
+				for (int i = 0; i < m; i++) {
+					sb.append(list[i]).append(" ");
+				}
+				sb.append("\n");
+			}
+			return;
+		}
+
+		int saveValue=-1;
+		for (int i = 1; i <= n; i++) {
+			if (visit[i] || saveValue==listvalue[i-1])
+				continue;
+			visit[i] = true;
+			list[cnt] = listvalue[i - 1];
+			saveValue = listvalue[i - 1];
+			dfs(cnt + 1);
+			visit[i] = false;
+		}
+	}
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
+
+		list = new int[9];
+		visit = new boolean[9];
+		listvalue = new int[n];
+
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < n; i++)
+			listvalue[i] = Integer.parseInt(st.nextToken());
+		sb = new StringBuilder();
+		Arrays.sort(listvalue);
+
+		dfs(0);
+
+		bw.write(sb.toString());
+
+		bw.close();
+		br.close();
+	}
+
+}
