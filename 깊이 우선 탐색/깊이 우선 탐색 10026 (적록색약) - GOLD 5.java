543edf5d09fde9d1770ba6c332dd95ef303fa02b
kimyoungjin12 <qazyj@naver.com>
2020-09-23 21:01:11 +0900

    dfs로 풀었다. 같은 문자면 ture해가면서 깊이 들어감. 적록색약은 R을 G로 모두 바꿔준다음에 똑같은 작업을 해주었다.

---
+++ 깊이 우선 탐색/깊이 우선 탐색 10026 (적록색약) - GOLD 5.java
@@ -0,0 +1,84 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Algorithm {
+	static int N;
+	static char[][] array;
+	static boolean[][] check;
+	static boolean colorWeakness = false;
+	static int[] x = { -1, 1, 0, 0 };
+	static int[] y = { 0, 0, -1, 1 };
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		N = Integer.parseInt(br.readLine());
+
+		array = new char[N][N];
+		check = new boolean[N][N];
+
+		for (int i = 0; i < N; i++) {
+			String s = br.readLine();
+			for (int j = 0; j < N; j++) {
+				array[i][j] = s.charAt(j);
+			}
+		}
+
+		int count = 0, colorWeeknessCount = 0;
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++) {
+				if (!check[i][j]) {
+					check[i][j] = true;
+					bfs(i, j, array[i][j]);
+					count++;
+				}
+			}
+		}
+
+		for (int i = 0; i < N; i++)
+			for (int j = 0; j < N; j++)
+				check[i][j] = false;
+
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++) {
+				if (array[i][j] == 'G')
+					array[i][j] = 'R';
+			}
+		}
+
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++) {
+				if (!check[i][j]) {
+					check[i][j] = true;
+					bfs(i, j, array[i][j]);
+					colorWeeknessCount++;
+				}
+			}
+		}
+
+		System.out.print(count + " " + colorWeeknessCount);
+	}
+
+	private static void bfs(int a, int b, char temp) {
+		Queue<int[]> queue = new LinkedList<>();
+		queue.offer(new int[] { a, b });
+
+		while (!queue.isEmpty()) {
+			int location[] = queue.poll();
+
+			for (int direction = 0; direction < 4; direction++) {
+				int r = location[0] + x[direction];
+				int c = location[1] + y[direction];
+
+				if (r >= 0 && c >= 0 && r < N && c < N) {
+					if (array[r][c] == temp && !check[r][c]) {
+						check[r][c] = true;
+						queue.offer(new int[] { r, c });
+					}
+				}
+			}
+		}
+	}
+}
