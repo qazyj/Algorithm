4bda7342de10a59c56460f571427a39fb25167f8
kimyoungjin12 <qazyj@naver.com>
2020-09-29 19:15:37 +0900

    한 노드를 기준으로 그 전의 행의 대각선(왼쪽, 오른쪽) 값을 더해서 최대가 되는 값을 현재 노드에 넣어주고 최대가 되는 노드의 값을 출력

---
+++ DP/DP 1932 (정수 삼각형) - SILVER 3.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Algorithm {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine()); 
+        int [][]dp = new int[N+1][N+1];
+        int max = 0;
+
+        for (int i = 1; i <= N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int j = 1; j <= i; j++) {
+                dp[i][j] = Integer.parseInt(st.nextToken());
+
+                dp[i][j] = Math.max(dp[i][j] + dp[i-1][j], dp[i][j] + dp[i-1][j-1]);
+                max = Math.max(dp[i][j], max);
+            }
+        }
+        
+        System.out.println(max);
+    }
+}
