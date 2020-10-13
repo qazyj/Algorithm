3cedd5503ac22753dd5bb86c0c7fa3b9a5364621
kimyoungjin12 <qazyj@naver.com>
2020-10-13 22:38:37 +0900

    KMP 알고리즘을 공부한 뒤 직접 구현해 보았다.

--- 문자열/문자열 16916 (부분 문자열) - GOLD 4.java
+++ 문자열/문자열 16916 (부분 문자열) - GOLD 4.java
@@ -1,61 +1,45 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+ 
+public class Main {	
+	static String firstString, secondString;
+	static int pi[];
+	
+	public static void main(String[] args) throws IOException { 
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		firstString = br.readLine();
+		secondString = br.readLine();
+		pi = new int[secondString.length()];
+				
+		System.out.println(KMP());
+	}
 
-public class Algorithm {
-	private static char[] s;
-	private static char[] p;
-
-    static int[] getPi(String pattern) {
-        int n = pattern.length();
-        int[] pi = new int[n];
-        char[] p = pattern.toCharArray();
-
-        pi[0] = 0;
-        int j = 0;
-        for(int i = 1; i<n; i++) {
-            while(j > 0 && p[i] != p[j]) {
-                j = pi[j-1];
-            }
-
-            if(p[i] == p[j]) {
-                pi[i] = j + 1;
-                j++;
-            }
-        }
-        return pi;
-    }
-
-    static int kmp (String source, String pattern) {
-        int n = source.length();
-        int m = pattern.length();
-        int[] pi = getPi(pattern);
-
-        int j = 0;
-        for(int i = 0; i<n; i++) {
-            while(j > 0 && s[i] != p[j]) {
-                j = pi[j-1];
-            }
-
-            if(s[i] == p[j]) {
-                if (j == m - 1) {
-                    return 1;
-                } else {
-                    j++;
-                }
-            }
-        }
-        return 0;
-    }
-
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        String source = br.readLine();
-        s = source.toCharArray();
-        String pattern = br.readLine();
-        p = pattern.toCharArray();
-
-        System.out.println(kmp(source, pattern));
-    }
+	// KMP 알고리즘 - pi[] 배열에서 주어진 문자열의 인덱스 0부터 i까지의 부분 문자열 중
+	// 접두사 == 접미사가 되는 가장 긴 접두사의 길이를 넣는다.
+	// 현재 밑 코드에서 접두사는 j(왼쪽), 접미사는 i(오른쪽)이다.
+	static int KMP() {
+		// j 접두사
+		int j = 0;
+		
+		// i 접미사
+	    for (int i = 1; i < secondString.length(); ++i) {
+	        while (j > 0 && secondString.charAt(i) != secondString.charAt(j)) j = pi[j - 1];
+	        if (secondString.charAt(i) == secondString.charAt(j)) pi[i] = ++j;
+	    }
+	    
+	    j = 0;
+	    for (int i = 0; i < firstString.length(); ++i) {
+	        while (j > 0 && firstString.charAt(i) != secondString.charAt(j)) j = pi[j - 1];
+	        if (firstString.charAt(i) == secondString.charAt(j)) {
+	            if (j == secondString.length() - 1) {
+	                return 1;
+	            }
+	            else ++j;
+	        }
+	    }
+	    return 0;
+	}
 }
