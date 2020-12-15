5f8cf0159c87730faa12941a61ba6c2209cf9077
kimyoungjin12 <qazyj@naver.com>
2020-12-15 12:37:06 +0900

    stack 부분을 deque로 고쳤다.

--- 그리디 알고리즘/그리디 2812 (크게 만들기) - GOLD 5.java
+++ 그리디 알고리즘/그리디 2812 (크게 만들기) - GOLD 5.java
@@ -1,23 +1,18 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.HashSet;
-import java.util.LinkedList;
-import java.util.PriorityQueue;
-import java.util.Queue;
-import java.util.Stack;
+import java.util.ArrayDeque;
+import java.util.Deque;
 import java.util.StringTokenizer;
 
-public class Algorithm {
+public class Main {
 	static int N, K;
-	static boolean[] check;
 	static char[] number;
+	static StringBuilder sb;
 
 	public static void main(String[] args) throws Exception {
 		SetData();
 		FindMaxValue();
+		System.out.println(sb);
 	}
 
 	private static void SetData() throws Exception {
@@ -26,34 +21,24 @@ public class Algorithm {
 
 		N = Integer.parseInt(st.nextToken());
 		K = Integer.parseInt(st.nextToken());
-		check = new boolean[N];
 		number = br.readLine().toCharArray();
+		sb = new StringBuilder();
 	}
 
 	private static void FindMaxValue() {
-		int temp = K;
-		Stack<Character> stack = new Stack<Character>();
+       		Deque<Character> deque = new ArrayDeque<>();
 		for (int i = 0; i < number.length; i++) {
 			// 스택의 맨 뒤의 값이 number[i]보다 작으면 삭제한다.
 			// 아래 조건을 만족할 때까지 반복.
-			while (temp > 0 && !stack.isEmpty() && stack.peek() < number[i]) {
-				stack.pop();
-				temp--;
+			while (K > 0 && !deque.isEmpty() && deque.getLast() < number[i]) {
+        				deque.removeLast();
+				K--;
 			}
-			stack.push(number[i]);
+			deque.addLast(number[i]);
 		}
 
-		StringBuilder sb = new StringBuilder();
-		if (stack.size() <= K) {
-			for (int i = 0; i < stack.size(); i++) {
-				sb.append(stack.get(i));
-			}
-		} else {
-			for(int i = 0; i < N-K;i++) {
-				sb.append(stack.get(i));
-			}
-		}
-
-		System.out.println(sb);
+	    while(deque.size() > K) {
+	       	sb.append(deque.removeFirst());
+	    }
 	}
 }
