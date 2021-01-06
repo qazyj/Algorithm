a5f9f06a460251f1299feb4091ee458bb0bd21cb
qazyj <qazyj@naver.com>
2021-01-07 00:00:44 +0900

    점화식으로 통해 구현하면 되는 문제이다. (구하고 싶은 index에서 index-1, index-2의 값을 더 해주면 된다.)

--- DP/DP11726 (2 x n 타일링) - SILVER 3.java
+++ DP/DP11726 (2 x n 타일링) - SILVER 3.java
@@ -1,26 +1,27 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Algorithm {
+	private static int N;
+	private static int[] dp;
+
+	public static void main(String[] args) throws Exception {
+		SetData();
+		System.out.println(dp[N]);
+	}
 	
-	static int n;	
-	static int[] dp;
-	
-	public static void main(String[] args) throws IOException {
-		// TODO Auto-generated method stub
+	private static void SetData() throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
-		n = Integer.parseInt(br.readLine());
-		dp = new int[1001];
+		N = Integer.parseInt(br.readLine());
+		dp = new int[N + 1];
 		
 		dp[1] = 1;
+		if(N == 1) return;
 		dp[2] = 2;
-		
-		for (int i = 3; i <1001 ; i ++) {	
-			dp[i] = (dp[i-1] + dp[i-2]) % 10007;
+		if(N == 2) return;
+		for (int i = 3; i <= N; i++) {
+			dp[i] = (dp[i - 1] + dp[i - 2]*2) % 10007;
 		}
-		
-		System.out.println(dp[n]);
 	}
 }
