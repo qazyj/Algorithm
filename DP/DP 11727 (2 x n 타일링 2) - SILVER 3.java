e46edb920cf7a1ac753398fb4a7e0afc681ddc87
qazyj <qazyj@naver.com>
2021-01-06 23:52:37 +0900

    점화식을 사용해서 푸는 문제이다. (런타임 뜨는 경우를 잘 생각하면서 코딩해야 한다!)

--- DP/DP 11727 (2 x n 타일링 2) - SILVER 3.java
+++ DP/DP 11727 (2 x n 타일링 2) - SILVER 3.java
@@ -2,23 +2,26 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
 public class Main {
+	private static int N;
+	private static int[] dp;
+
 	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int N = Integer.parseInt(br.readLine());
-		if (N == 1) {
-			System.out.println(1);
-			System.exit(0);
-		}
-		if (N == 2) {
-			System.out.println(3);
-			System.exit(0);
-		}
-		int[] dp = new int[N + 1];
-		dp[1] = 1;
-		dp[2] = 3;
-		for (int i = 3; i <= N; i++) {
-			dp[i] = (dp[i - 1] + dp[i - 2] + i) % 10007;
-		}
+		SetData();
 		System.out.println(dp[N]);
+	}
+	
+	private static void SetData() throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		N = Integer.parseInt(br.readLine());
+		dp = new int[N + 1];
+		
+		dp[1] = 1;
+		if(N == 1) return;
+		dp[2] = 3;
+		if(N == 2) return;
+		for (int i = 3; i <= N; i++) {
+			dp[i] = (dp[i - 1] + dp[i - 2]*2) % 10007;
+		}
 	}
 }
