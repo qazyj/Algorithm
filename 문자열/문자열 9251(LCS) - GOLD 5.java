a8eb745e49e170c1d3d19877f3d6ea8dd45c36ec
qazyj <qazyj@naver.com>
2021-01-12 23:09:28 +0900

    LCS 알고리즘의 점화식을 통하여 구현하였다

--- 문자열/문자열 9251(LCS) - GOLD 5.java
+++ 문자열/문자열 9251(LCS) - GOLD 5.java
@@ -1,31 +1,34 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class Algorithm {
+public class Main {
+	static int[][] dp;
+	static int firstInputLength, secondInputLength;
 
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws Exception {
 		// TODO Auto-generated method stub
+		SetData();
+		System.out.println(dp[firstInputLength][secondInputLength]);
+	}
+	
+	private static void SetData() throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
 		String firstInput = br.readLine();
 		String secondInput = br.readLine();
 
-		int[][] dp = new int[firstInput.length() + 1][secondInput.length() + 1]; // 각 문자열의 길이만큼 선언
-		// 주의해야 할 점은 무엇을 기준으로 무엇과 비교할지를 정한 후 순서에 맞게 선언해주어야 한다.
-		// 여기선 b를 기준으로 a와 비교하게 된다.
-
-		for (int i = 1; i <= firstInput.length(); i++) { 
-			for (int j = 1; j <= secondInput.length(); j++) { 
+		firstInputLength = firstInput.length();
+		secondInputLength = secondInput.length();
+		dp = new int[firstInputLength + 1][secondInputLength + 1];
+		
+		for (int i = 1; i <= firstInputLength; i++) { 
+			for (int j = 1; j <= secondInputLength; j++) { 
 				if (firstInput.charAt(i - 1) == secondInput.charAt(j - 1)) { 
-					dp[i][j] = dp[i - 1][j - 1] + 1; // 점화식
+					dp[i][j] = dp[i - 1][j - 1] + 1;
 				} else { 
-					dp[i][j] = Math.max(dp[i][j - 1], dp[i - 1][j]); // 점화식					
+					dp[i][j] = Math.max(dp[i][j - 1], dp[i - 1][j]);				
 				}
 			}
 		}
-
-		System.out.println(dp[firstInput.length()][secondInput.length()]); // 이곳 역시 선언과 똑같이 출력해주어야 런타임 에러가 나지 않는다.
-
-	}
+	}	
 }
