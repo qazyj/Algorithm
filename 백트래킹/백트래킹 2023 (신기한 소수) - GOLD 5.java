83dcd148be7387751ebc794e2eb3d84fb286287f
qazyj <qazyj@naver.com>
2021-01-18 18:00:56 +0900

    DFS를 통해 N자리의 각 자리 모두 소수인 숫자만 추가해주었다.

--- 백트래킹/백트래킹 2023 (신기한 소수) - GOLD 5.java
+++ 백트래킹/백트래킹 2023 (신기한 소수) - GOLD 5.java
@@ -1,37 +1,43 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
-public class Algorithm {
+public class Main {
 	static int N;
 	static StringBuilder sb;
 
 	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		N = Integer.parseInt(br.readLine());
-		sb = new StringBuilder();
-		
-		dfs("");
+		SetData();
 		System.out.println(sb.toString());
 	}
 
-	static void dfs(String s) {
-		if (s.length() == N) {
-			sb.append(s+'\n');
-			return;
-		}
-		for(int i=1; i<=9; i++) {
-			if(Prime(Integer.parseInt(s+i))) {
-				dfs(s+i);
-			}
-		}
-		
+	private static void SetData() throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		N = Integer.parseInt(br.readLine());
+		sb = new StringBuilder();
+
+		dfs("");
 	}
 
-	static boolean Prime(int num) {
-		if(num==1) return false;
-		
-		for(int i=2; i<=Math.sqrt(num); i++) {
-			if(num%i==0) return false;
+	private static void dfs(String s) {
+		if (s.length() == N) {
+			sb.append(s + '\n');
+			return;
+		}
+		for (int i = 1; i <= 9; i++) {
+			if (Prime(Integer.parseInt(s + i))) {
+				dfs(s + i);
+			}
+		}
+
+	}
+
+	private static boolean Prime(int num) {
+		if (num == 1)
+			return false;
+
+		for (int i = 2; i <= Math.sqrt(num); i++) {
+			if (num % i == 0)
+				return false;
 		}
 		return true;
 	}
