02c2e9a09d7f4b57508d13ed6c07f8f2d93f4cb1
qazyj <qazyj@naver.com>
2021-01-18 18:04:51 +0900

    현재 계단이 i일 때, 그 전 계단 i-1을 밟고 올라왔는지 아닌지로 나눠서 계산한다. 계단을 밟는 개수는 최소가 되어야 하므로 전 단계 i-1을 밟았으면 그 전전 단계 i-2는 점프했다고 가정한다. 그래서 현재 계단까지의 최대값 dp[i]는 지금 계단의 값 steps[i], 전 단계 steps[i-1]과 점프 전 최대값 dp[i-3]의 합이다. 만약 전 단계 i-1을 밟지않고 점프했다면, dp[i]는 현재 값 steps[i]와 점프한 최대값 dp[i-2]의 합이다. 이 둘 중 큰 값을 dp[i]에 저장하면 된다.

--- DP/DP 2579 (계단 오르기) - SILVER 3.java
+++ DP/DP 2579 (계단 오르기) - SILVER 3.java
@@ -1,27 +1,35 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
-public class Main {
+public class Algorithm {
+	static int n;
+	static int[] dp;
+
 	public static void main(String[] args) throws Exception {
+		SetData();
+		System.out.println(dp[n]);
+	}
+
+	private static void SetData() throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int n = Integer.parseInt(br.readLine());
+		
+		n = Integer.parseInt(br.readLine());
 		int[] steps = new int[n+1];
-		int[] dp = new int[n+1];
+		dp = new int[n+1];
 		int noJump = 0, jump = 0;
+		
 		for(int i = 1; i <= n; i++)
 			steps[i] = Integer.parseInt(br.readLine());
 		
 		dp[1] = steps[1];
-		dp[2] = steps[1] + steps[2];
+		if(n>=2)
+			dp[2] = steps[1] + steps[2];
+		
 		for(int i = 3; i <= n; i++) {
 			noJump = steps[i] + steps[i-1] + dp[i-3];
 			jump = steps[i] + dp[i-2];
 					
 			dp[i] = Math.max(noJump, jump);
 		}
-		
-		System.out.println(dp[n]);
 	}
 }
-
-
