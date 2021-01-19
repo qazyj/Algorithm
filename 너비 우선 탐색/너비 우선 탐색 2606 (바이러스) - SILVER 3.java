8f95d815806e050d1ff161ad89f696b1949a8e2d
qazyj <qazyj@naver.com>
2021-01-19 17:55:40 +0900

    컴퓨터 1과 연결되어 있으면 계속 깊이탐색을 해서 연결된 컴퓨터 갯수를 카운트하면 답은 쉽게 나온다.

--- 너비 우선 탐색/너비 우선 탐색 2606 (바이러스) - SILVER 3.java
+++ 너비 우선 탐색/너비 우선 탐색 2606 (바이러스) - SILVER 3.java
@@ -1,16 +1,19 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-
-	static int a, b, result;
+	static int a, b, answer;
 	static boolean[][] graph;	
 	static boolean[] check;	
 
-	public static void main(String[] agrs) throws IOException {
+	public static void main(String[] args) throws Exception {
+		SetData();
+		dfs(1);
+		System.out.println(answer);
+	}
 
+	private static void SetData() throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
 		a = Integer.parseInt(br.readLine());
@@ -20,28 +23,21 @@ public class Main {
 		check = new boolean[a + 1];
 
 		for (int i = 1; b >= i; ++i) {
-			String s = br.readLine();
-			StringTokenizer st = new StringTokenizer(s);
+			StringTokenizer st = new StringTokenizer(br.readLine());
 			int first = Integer.parseInt(st.nextToken());
 			int second = Integer.parseInt(st.nextToken());
 			graph[first][second] = true;
 			graph[second][first] = true;
-		}		
-
-		dfs(1);
-		System.out.println(result);
-
+		}
 	}
-
+	
 	static void dfs(int start) {
 		check[start] = true;
 		for (int i = 1; a >= i; ++i) {	        								
 			if (graph[start][i] == true && check[i] == false) {
 					dfs(i);		
-					result++;	
+					answer++;	
 				}
 			}
 		}
-
 }
- 
