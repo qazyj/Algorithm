2f87e056cecf71b533d3d44970763c5985219691
qazyj <qazyj@naver.com>
2021-01-20 21:36:48 +0900

    backtraking 메소드에서 문자열에 1,2,3을 계속 추가시키면서 좋을수열(PossibleNumber 메소드에서 체크)인 수열만 계속 backtraking하다가 수의 길이가 N이면 출력 후 종료(제일 적은 수가 먼저 도착하기 때문)

--- 백트래킹/백트래킹 2661 (좋은수열) - GOLD 4.java
+++ 백트래킹/백트래킹 2661 (좋은수열) - GOLD 4.java
@@ -1,20 +1,20 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
 
 public class Algorithm {
 	static int N;
 
 	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-		N = Integer.parseInt(br.readLine());
-
+		SetData();
 		backtracking("");
 	}
 
+	private static void SetData() throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		N = Integer.parseInt(br.readLine());
+	}
+	
 	private static void backtracking(String s) {
 		if (s.length() == N) {
 			System.out.println(s);
@@ -26,14 +26,12 @@ public class Algorithm {
 				backtracking(s + i);
 		}
 	}
-
+	
 	private static boolean PossibleNumber(String s) {
 		int length = s.length();
 		for (int i = 1; i <= length/2; i++) {
 			String frontString = s.substring(length-i*2, length-i);
 			String backString = s.substring(length-i, length);
-			if(length == 7)
-				System.out.println(frontString + " " + backString);
 			if (frontString.equals(backString))
 				return false;
 		}
