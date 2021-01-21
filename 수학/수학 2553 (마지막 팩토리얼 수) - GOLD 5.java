bba65a482217ba5dfaac0d4c3f4b68f0d5a55f7b
qazyj <qazyj@naver.com>
2021-01-21 19:04:11 +0900

    19996~19999까지 곱하게 된다면 5자리수 * 5자리수 * 5자리수 * 5자리수 이므로 숫자가 조단위가 됩니다. 이 점에 유의하셔야 시간초과를 피할 수 있다.

--- 수학/수학 2553 (마지막 팩토리얼 수) - GOLD 5.java
+++ 수학/수학 2553 (마지막 팩토리얼 수) - GOLD 5.java
@@ -1,25 +1,25 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.StringTokenizer;
 
-public class Main {
-	
-	public static void main(String[] args) throws IOException {
-		// TODO Auto-generated method stub        
+public class Algorithm {
+	static long answer;
+
+	public static void main(String[] args) throws Exception {
+		SetData();
+		System.out.println(answer%10);
+	}
+
+	private static void SetData() throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
 		long input = Long.parseLong(br.readLine());
-		long m =1;
+		answer =1;
 		
 		for(int i=1;i<=input;i++) {
-			m*=i;
-			m %= 1000000000;
-			while (m % 10 == 0)
-			     m /= 10;
+			answer*=i;
+			answer %= 1000000000;
+			while (answer % 10 == 0)
+			     answer /= 10;
 		}
-		 System.out.println(m%10);
-    }
+	}
 }
