90b09305e6a222d6ada2d0c5df0165afba548286
qazyj <qazyj@naver.com>
2021-01-22 15:30:01 +0900

    우선순위 큐를 이용하여 풀었다. (stream 처음 사용)

--- 그리디 알고리즘/그리디 1781 (컵라면) - GOLD 1.java
+++ 그리디 알고리즘/그리디 1781 (컵라면) - GOLD 1.java
@@ -1,11 +1,11 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.Arrays;
 import java.util.Comparator;
+import java.util.InputMismatchException;
 import java.util.PriorityQueue;
-import java.util.StringTokenizer;
 
-public class Algorithm {
+public class Main {
 	static int N;
 	static int[][] array;
 
@@ -15,16 +15,14 @@ public class Algorithm {
 	}
 	
 	private static void SetData() throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = null;
+		InputReader in = new InputReader(System.in);
 		
-		N = Integer.parseInt(br.readLine());
+		N = in.readInt();
 		array = new int[N][2];
 		
 		for(int i = 0; i < N; i++) {
-			st = new StringTokenizer(br.readLine());
-			array[i][0] = Integer.parseInt(st.nextToken());
-			array[i][1] = Integer.parseInt(st.nextToken());
+			array[i][0] = in.readInt();
+			array[i][1] = in.readInt();
 		}
 		
 		// 2차원 배열 정렬 (deadline 오름차순)
@@ -41,10 +39,74 @@ public class Algorithm {
         int sum = 0;
         for (int i = 0; i < N; i++) {
             queue.add(array[i][1]);
-            while (!queue.isEmpty() && queue.size() > array[i][0]) queue.poll();
+            while (!queue.isEmpty() && queue.size() > array[i][0]) 
+            	queue.poll();
         }
         while (!queue.isEmpty())
             sum += queue.poll();
 		return sum;
+	}
+	
+	private static class InputReader {
+		private InputStream stream;
+		private byte[] buf = new byte[1024];
+		private int curChar;
+		private int numChars;
+		private SpaceCharFilter filter;
+
+		public InputReader(InputStream stream) {
+			this.stream = stream;
+		}
+
+		public int read() {
+			if (numChars == -1) {
+				throw new InputMismatchException();
+			}
+			if (curChar >= numChars) {
+				curChar = 0;
+				try {
+					numChars = stream.read(buf);
+				} catch (IOException e) {
+					throw new InputMismatchException();
+				}
+				if (numChars <= 0) {
+					return -1;
+				}
+			}
+			return buf[curChar++];
+		}
+
+		public int readInt() {
+			int c = read();
+			while (isSpaceChar(c)) {
+				c = read();
+			}
+			int sgn = 1;
+			if (c == '-') {
+				sgn = -1;
+				c = read();
+			}
+			int res = 0;
+			do {
+				if (c < '0' || c > '9') {
+					throw new InputMismatchException();
+				}
+				res *= 10;
+				res += c - '0';
+				c = read();
+			} while (!isSpaceChar(c));
+			return res * sgn;
+		}
+
+		public boolean isSpaceChar(int c) {
+			if (filter != null) {
+				return filter.isSpaceChar(c);
+			}
+			return c == ' ' || c == '\n' || c == '\r' || c == '\t' || c == -1;
+		}
+
+		public interface SpaceCharFilter {
+			public boolean isSpaceChar(int ch);
+		}
 	}
 }
