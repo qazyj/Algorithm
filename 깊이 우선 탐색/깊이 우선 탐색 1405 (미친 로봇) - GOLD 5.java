c6ab444b2d8b3846d18a0d300eeeed5d57487022
qazyj <qazyj@naver.com>
2021-01-26 21:25:59 +0900

    DFS 방식으로 구현하였다.

--- 깊이 우선 탐색/깊이 우선 탐색 1405 (미친 로봇) - GOLD 5.java
+++ 깊이 우선 탐색/깊이 우선 탐색 1405 (미친 로봇) - GOLD 5.java
@@ -1,6 +1,6 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.InputMismatchException;
 
 public class Algorithm {
 	static int N;
@@ -11,26 +11,30 @@ public class Algorithm {
 	static int[] y = { 0, 0, -1, 1 };
 
 	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		percent = new double[4];
-		check = new boolean[30][30];
-		percentage = 0.0;
-
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		N = Integer.parseInt(st.nextToken());
-
-		// ÀÔ·Â°ªÀ» È®·ü·Î ¹Ù²Þ
-		for (int i = 0; i < 4; i++) 
-			percent[i] = Integer.parseInt(st.nextToken()) * 0.01;
-		
-		check[14][14] = true;
+		SetData();
 		dfs(14, 14, 0, 1.0);
-		
 		System.out.println(percentage);
 	}
 
+	// µ¥ÀÌÅÍ
+	private static void SetData() throws Exception {
+		InputReader in = new InputReader(System.in);
+
+		percent = new double[4];
+		check = new boolean[30][30];
+		percentage = 0.0;
+		check[14][14] = true;	
+
+		N = in.readInt();		
+
+		// ÀÔ·Â°ªÀ» È®·ü·Î ¹Ù²Þ
+		for (int i = 0; i < 4; i++) 
+			percent[i] = in.readInt() * 0.01;
+		
+	}
+
 	private static void dfs(int a, int b, int count, double per) {
+		// basecase
 		if (count == N) {
 			percentage += per;
 			return;
@@ -45,6 +49,69 @@ public class Algorithm {
 				dfs(r, c, count + 1, per * percent[i]);
 				check[r][c] = false;
 			}
+		}
+	}
+	
+	private static class InputReader {
+		private InputStream stream;
+		private byte[] buf = new byte[1024];
+		private int curChar;
+		private int numChars;
+		private SpaceCharFilter filter;
+
+		public InputReader(InputStream stream) {
+			this.stream = stream;
+		}
+
+		public int read() {
+			if (numChars == -1) {
+				throw new InputMismatchException();
+			}
+			if (curChar >= numChars) {
+				curChar = 0;
+				try {
+					numChars = stream.read(buf);
+				} catch (IOException e) {
+					throw new InputMismatchException();
+				}
+				if (numChars <= 0) {
+					return -1;
+				}
+			}
+			return buf[curChar++];
+		}
+
+		public int readInt() {
+			int c = read();
+			while (isSpaceChar(c)) {
+				c = read();
+			}
+			int sgn = 1;
+			if (c == '-') {
+				sgn = -1;
+				c = read();
+			}
+			int res = 0;
+			do {
+				if (c < '0' || c > '9') {
+					throw new InputMismatchException();
+				}
+				res *= 10;
+				res += c - '0';
+				c = read();
+			} while (!isSpaceChar(c));
+			return res * sgn;
+		}
+
+		public boolean isSpaceChar(int c) {
+			if (filter != null) {
+				return filter.isSpaceChar(c);
+			}
+			return c == ' ' || c == '\n' || c == '\r' || c == '\t' || c == -1;
+		}
+
+		public interface SpaceCharFilter {
+			public boolean isSpaceChar(int ch);
 		}
 	}
 }
