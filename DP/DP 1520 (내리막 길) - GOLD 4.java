ee4ee05a80b5e30f30e69bbf5f48ce38bba61c77
qazyj <qazyj@naver.com>
2021-01-27 12:04:50 +0900

    dp배열을 하나 더 만들어서 지나온 길의 값을 저장해주고 사용하면서 bfs를 통해 배열을 훑는다.

--- DP/DP 1520 (내리막 길) - GOLD 4.java
+++ DP/DP 1520 (내리막 길) - GOLD 4.java
@@ -1,36 +1,37 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.InputMismatchException;
 
-public class Main {
+public class Algorithm {
 	private static int N, M;
 	private static int[][] array, dp;
-
+	
 	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		M = Integer.parseInt(st.nextToken());
-		N = Integer.parseInt(st.nextToken());
+		SetData();
+		System.out.println(bfs(0,0));
+	}
+
+	// 데이터
+	private static void SetData() throws Exception {
+		InputReader in = new InputReader(System.in);
+		
+        M = in.nextInt();
+		N = in.nextInt();
 		array = new int[M][N];
 		dp = new int[M][N];
 
 		for (int i = 0; i < M; i++) {
-			st = new StringTokenizer(br.readLine());
 			for (int j = 0; j < N; j++) {
-				array[i][j] = Integer.parseInt(st.nextToken());
+				array[i][j] = in.nextInt();
 				dp[i][j] = -1;
 			}
 		}
-
-		System.out.println(bfs(0,0));
+		
 	}
 
 	// 메모리 초과때문에 메모리 줄이기 위함
 	private static int[][] xy = { { 1, 0 }, { 0, 1 }, { -1, 0 }, { 0, -1 } };
-
+	
 	// 메모리 초과 때문에 bfs를 queue에서 재귀로 바꿈
 	private static int bfs(int a, int b) {
 		// base case
@@ -51,8 +52,113 @@ public class Main {
 					
 				}
 			}
-
 		}
 		return dp[a][b];
+	}
+}
+
+class InputReader {
+
+	private final InputStream stream;
+	private final byte[] buf = new byte[8192];
+	private int curChar, snumChars;
+
+	public InputReader(InputStream st) {
+		this.stream = st;
+	}
+
+	public int read() {
+		if (snumChars == -1)
+			throw new InputMismatchException();
+		if (curChar >= snumChars) {
+			curChar = 0;
+			try {
+				snumChars = stream.read(buf);
+			} catch (IOException e) {
+				throw new InputMismatchException();
+			}
+			if (snumChars <= 0)
+				return -1;
+		}
+		return buf[curChar++];
+	}
+
+	public int nextInt() {
+		int c = read();
+		while (isSpaceChar(c)) {
+			c = read();
+		}
+		int sgn = 1;
+		if (c == '-') {
+			sgn = -1;
+			c = read();
+		}
+		int res = 0;
+		do {
+			res *= 10;
+			res += c - '0';
+			c = read();
+		} while (!isSpaceChar(c));
+		return res * sgn;
+	}
+
+	public long nextLong() {
+		int c = read();
+		while (isSpaceChar(c)) {
+			c = read();
+		}
+		int sgn = 1;
+		if (c == '-') {
+			sgn = -1;
+			c = read();
+		}
+		long res = 0;
+		do {
+			res *= 10;
+			res += c - '0';
+			c = read();
+		} while (!isSpaceChar(c));
+		return res * sgn;
+	}
+
+	public int[] nextIntArray(int n) {
+		int a[] = new int[n];
+		for (int i = 0; i < n; i++) {
+			a[i] = nextInt();
+		}
+		return a;
+	}
+
+	public String readString() {
+		int c = read();
+		while (isSpaceChar(c)) {
+			c = read();
+		}
+		StringBuilder res = new StringBuilder();
+		do {
+			res.appendCodePoint(c);
+			c = read();
+		} while (!isSpaceChar(c));
+		return res.toString();
+	}
+
+	public String nextLine() {
+		int c = read();
+		while (isSpaceChar(c))
+			c = read();
+		StringBuilder res = new StringBuilder();
+		do {
+			res.appendCodePoint(c);
+			c = read();
+		} while (!isEndOfLine(c));
+		return res.toString();
+	}
+
+	public boolean isSpaceChar(int c) {
+		return c == ' ' || c == '\n' || c == '\r' || c == '\t' || c == -1;
+	}
+
+	private boolean isEndOfLine(int c) {
+		return c == '\n' || c == '\r' || c == -1;
 	}
 }
