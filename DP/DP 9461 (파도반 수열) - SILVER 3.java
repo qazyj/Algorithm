9803c37e5bf7dc3d5691e098ab428e67405ea48e
qazyj <qazyj@naver.com>
2021-01-29 15:53:25 +0900

    DP문제이다. index 5까지만 반복해서 보면 dp[i] = di[i-2] + dp[i-3]이 점화식이 되는 것을 쉽게 알 수 있다.

--- DP/DP 9461 (파도반 수열) - SILVER 3.java
+++ DP/DP 9461 (파도반 수열) - SILVER 3.java
@@ -1,21 +1,127 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.InputMismatchException;
 
 public class Algorithm {
+	static StringBuilder sb;
+	static long[] dp;
 	
-    public static void main(String[] args) throws Exception {
-    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-        
-        long[] dp = new long[101];
-        dp[0] = 0;
+	public static void main(String[] args) throws Exception {
+		SetData();
+		System.out.println(sb);
+	}
+
+	// 데이터
+	private static void SetData() throws Exception {
+		InputReader in = new InputReader(System.in);
+
+		int N = in.nextInt();    
+		sb = new StringBuilder();
+        dp = new long[101];
         dp[1] = 1;
         dp[2] = 1;
         dp[3] = 1;
+        
+        Solve(in, N);
+	}
+	
+	private static void Solve(InputReader in, int N) {
         for(int i = 4; i < 101; i++) 
         	dp[i] = dp[i-2] + dp[i-3];
         
         for(int i = 0; i< N; i++)
-        	System.out.println(dp[Integer.parseInt(br.readLine())]);
-    } 
+        	sb.append(dp[in.nextInt()]).append("\n");
+	}
+}
+
+class InputReader {
+	private final InputStream stream;
+	private final byte[] buf = new byte[8192];
+	private int curChar, snumChars;
+
+	public InputReader(InputStream st) {
+		this.stream = st;
+	}
+
+	public int read() {
+		if (snumChars == -1)
+			throw new InputMismatchException();
+		if (curChar >= snumChars) {
+			curChar = 0;
+			try {
+				snumChars = stream.read(buf);
+			} catch (IOException e) {
+				throw new InputMismatchException();
+			}
+			if (snumChars <= 0)
+				return -1;
+		}
+		return buf[curChar++];
+	}
+
+	public int nextInt() {
+		int c = read();
+		while (isSpaceChar(c)) {
+			c = read();
+		}
+		int sgn = 1;
+		if (c == '-') {
+			sgn = -1;
+			c = read();
+		}
+		int res = 0;
+		do {
+			res *= 10;
+			res += c - '0';
+			c = read();
+		} while (!isSpaceChar(c));
+		return res * sgn;
+	}
+
+	public long nextLong() {
+		int c = read();
+		while (isSpaceChar(c)) {
+			c = read();
+		}
+		int sgn = 1;
+		if (c == '-') {
+			sgn = -1;
+			c = read();
+		}
+		long res = 0;
+		do {
+			res *= 10;
+			res += c - '0';
+			c = read();
+		} while (!isSpaceChar(c));
+		return res * sgn;
+	}
+
+	public int[] nextIntArray(int n) {
+		int a[] = new int[n];
+		for (int i = 0; i < n; i++) {
+			a[i] = nextInt();
+		}
+		return a;
+	}
+
+	public String nextLine() {
+		int c = read();
+		while (isSpaceChar(c))
+			c = read();
+		StringBuilder res = new StringBuilder();
+		do {
+			res.appendCodePoint(c);
+			c = read();
+		} while (!isEndOfLine(c));
+		return res.toString();
+	}
+
+	public boolean isSpaceChar(int c) {
+		return c == ' ' || c == '\n' || c == '\r' || c == '\t' || c == -1;
+	}
+
+	private boolean isEndOfLine(int c) {
+		return c == '\n' || c == '\r' || c == -1;
+	}
 }
