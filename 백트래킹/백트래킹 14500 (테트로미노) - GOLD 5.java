7627e4b3d26f728dc07b28e44b42dd6d9185c20d
qazyj <qazyj@naver.com>
2021-02-08 18:02:28 +0900

    dfs 알고리즘을 통해서 모든 모양을 테스트함

--- 백트래킹/백트래킹 14500 (테트로미노) - GOLD 5.java
+++ 백트래킹/백트래킹 14500 (테트로미노) - GOLD 5.java
@@ -1,85 +1,305 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.InputMismatchException;
 
 public class Main {
-
-	static int N, M, result;
-    static int[][] array;
-	static Boolean[][] check;
-	static int[]x = {0, 0, 1, -1};
-	static int[]y = {-1, 1, 0, 0};
-
-	static int [][]shapeX = {{ 0, 0, 0, 1 }, { 0, 1, 2, 1 }, { 0, 0, 0, -1 }, { 0, -1, 0, 1 }};
-	static int [][]shapeY = {{ 0, 1, 2, 1 }, { 0, 0, 0, 1 }, { 0, 1, 2, 1 }, { 0, 1, 1, 1 }};
+	static int M, N, answer, array [][];
 
 	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());		
+		SetData();
+		System.out.println(answer);
+	}
 
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
+	private static void SetData() throws Exception {
+		InputReader in = new InputReader(System.in);
 
-		array = new int[N][M];
-		check = new Boolean[N][M];
-		for (int i = 0; i < N; i++) 
-			for (int j = 0; j < M; j++) 
-				check[i][j] = false;
-
-		for (int i = 0; i < N; i++) {
-			st = new StringTokenizer(br.readLine());
-			for (int j = 0; j < M; j++) {
-				array[i][j] = Integer.parseInt(st.nextToken());
+		int N = in.nextInt();
+		int M = in.nextInt();
+		array = new int[M+6][N+6];
+		for(int i = 3; i<N+3; i++){
+			for(int j = 3; j<M+3; j++){
+				array[j][i] = in.nextInt();
 			}
 		}
 		
-		for (int i = 0; i < N; i++) {
-			for (int j = 0; j < M; j++) {
-				check[i][j] = true;
-				dfs(i, j, array[i][j], 1);
-				check[i][j] = false;
-				shape(i, j);
+		for(int i=0; i<M+3; i++){
+			for(int j=0; j<N+3; j++){
+				dfs(i, j);
 			}
+		}
+	}
+	
+	private static void dfs(int a, int b){		
+		// 직선 (세로 놓기)
+		int sum=0;
+		sum += array[a][b];
+		sum += array[a+1][b];
+		sum += array[a+2][b];
+		sum += array[a+3][b];
+		if(answer<sum){
+			answer = sum;
 		}
 		
-		System.out.println(result);
+		// 직선 (가로 놓기)
+		sum=0;
+		sum += array[a][b];
+		sum += array[a][b+1];
+		sum += array[a][b+2];
+		sum += array[a][b+3];
+		if(answer<sum){
+			answer = sum;
+		} 
+		
+		// 네모
+		sum=0;
+		sum += array[a][b];
+		sum += array[a+1][b];
+		sum += array[a+1][b+1];
+		sum += array[a][b+1];
+		if(answer<sum){
+			answer = sum;
+		}
+		// ㄴ  // 왼상단시작 오른 하단 종료. (반시계방향)
+		sum=0;
+		sum += array[a][b];
+		sum += array[a+1][b];
+		sum += array[a+2][b];
+		sum += array[a+2][b+1];
+		if(answer<sum){
+			answer = sum;
+		}
+		// ㄴ case // 왼상단시작 오른 하단 종료. (반시계방향)의 대칭.
+		sum=0;
+		sum += array[a][b+1];
+		sum += array[a+1][b+1];
+		sum += array[a+2][b+1];
+		sum += array[a+2][b];
+		if(answer<sum){
+			answer = sum;
+		}
+
+		// ㄴ  // 왼하단시작 오른 상단 종료.
+		sum=0;
+		sum += array[a][b];
+		sum += array[a+1][b];
+		sum += array[a][b+1];
+		sum += array[a][b+2];
+		if(answer<sum){
+			answer = sum;
+		}
+		// ㄴ  // 왼하단시작 오른 상단 종료.
+		sum=0;
+		sum += array[a][b];
+		sum += array[a+1][b+2];
+		sum += array[a][b+1];
+		sum += array[a][b+2];
+		if(answer<sum){
+			answer = sum;
+		}
+
+
+		// ㄴ  // 왼상단시작 오른 하단 종료 (시계방향) 
+		sum=0;
+		sum += array[a][b];
+		sum += array[a][b+1];
+		sum += array[a+1][b+1];
+		sum += array[a+2][b+1];
+		if(answer<sum){
+			answer = sum;
+		}
+		// ㄴ  // 왼상단시작 오른 하단 종료 (시계방향) 의 대칭
+		sum=0;
+		sum += array[a][b];
+		sum += array[a][b+1];
+		sum += array[a+1][b];
+		sum += array[a+2][b];
+		if(answer<sum){
+			answer = sum;
+		}
+		//  ㄴ  // 오른 상단시작  왼 하단 종료
+		sum=0;
+		sum += array[a][b+2];
+		sum += array[a+1][b+2];
+		sum += array[a+1][b+1];
+		sum += array[a+1][b];
+		if(answer<sum){
+			answer = sum;
+		}
+		// ㄴ  // 오른 상단시작  왼 하단 종료 의 대칭  
+		sum=0;
+		sum += array[a+1][b+2];
+		sum += array[a+1][b+1];
+		sum += array[a+1][b];
+		sum += array[a][b];
+		if(answer<sum){
+			answer = sum;
+		}
+		
+		// ㄴㄱ  // 왼상단시작 오른 하단 종료
+		sum=0;
+		sum += array[a][b];
+		sum += array[a+1][b];
+		sum += array[a+1][b+1];
+		sum += array[a+2][b+1];
+		if(answer<sum){
+			answer = sum;
+		}
+		// ㄴㄱ  // 오른 상단시작  하단 종료. 
+		sum=0;
+		sum += array[a][b+2];
+		sum += array[a][b+1];
+		sum += array[a+1][b+1];
+		sum += array[a+1][b];
+		if(answer<sum){
+			answer = sum;
+		}
+		// ㄴㄱ  // 왼하단시작 오른 상단 종료. 
+		sum=0;
+		sum += array[a+2][b];
+		sum += array[a+1][b];
+		sum += array[a+1][b+1];
+		sum += array[a][b+1];
+		if(answer<sum){
+			answer = sum;
+		}
+		// ㄴㄱ  // 왼상단시작 오른 하단 종료.  (ㄱㄴ꼴)
+		sum=0;
+		sum += array[a][b];
+		sum += array[a][b+1];
+		sum += array[a+1][b+1];
+		sum += array[a+1][b+2];
+		if(answer<sum){
+			answer = sum;
+		}
+		// ㅗ   //  ㅜ
+		sum=0;
+		sum += array[a][b];
+		sum += array[a][b+1];
+		sum += array[a][b+2];
+		sum += array[a+1][b+1];
+		if(answer<sum){
+			answer = sum;
+		}
+		// ㅗ   // ㅓ  
+		sum=0;
+		sum += array[a][b+1];
+		sum += array[a+1][b+1];
+		sum += array[a+2][b+1];
+		sum += array[a+1][b];
+		if(answer<sum){
+			answer = sum;
+		}
+		// ㅗ   //  ㅗ  
+		sum=0;
+		sum += array[a+1][b];
+		sum += array[a+1][b+1];
+		sum += array[a+1][b+2];
+		sum += array[a][b+1];
+		if(answer<sum){
+			answer = sum;
+		}	
+		// ㅗ   //  ㅏ  
+		sum=0;
+		sum += array[a][b];
+		sum += array[a+1][b];
+		sum += array[a+2][b];
+		sum += array[a+1][b+1];
+		if(answer<sum){
+			answer = sum;
+		}
+
+
+	}
+}
+
+class InputReader {
+	private final InputStream stream;
+	private final byte[] buf = new byte[8192];
+	private int curChar, snumChars;
+
+	public InputReader(InputStream st) {
+		this.stream = st;
 	}
 
-	public static void dfs(int a, int b, int sum, int length) {
-		if (length >= 4) {
-			result = Math.max(result, sum);
-			return;
-		}
-
-		for (int direction = 0; direction < 4; direction++) {
-			int r = a + x[direction];
-			int c = b + y[direction];
-
-			if (r >= 0 && c >= 0 && r < N && c < M) {
-				if (!check[r][c]) {
-					check[r][c] = true;
-					dfs(r, c, sum + array[r][c], length + 1);
-					check[r][c] = false;
-				}
+	public int read() {
+		if (snumChars == -1)
+			throw new InputMismatchException();
+		if (curChar >= snumChars) {
+			curChar = 0;
+			try {
+				snumChars = stream.read(buf);
+			} catch (IOException e) {
+				throw new InputMismatchException();
 			}
+			if (snumChars <= 0)
+				return -1;
 		}
+		return buf[curChar++];
 	}
 
-	public static void shape(int x, int y) {
-
-		for (int i = 0; i < 4; i++) {
-			int sum = 0;
-
-			for (int j = 0; j < 4; j++) {
-				int r = x + shapeX[i][j];
-				int c = y + shapeY[i][j];
-
-				if (r >= 0 && c >= 0 && r < N && c < M) {
-					sum += array[r][c];
-				}
-			}
-			
-			result = Math.max(result, sum);
+	public int nextInt() {
+		int c = read();
+		while (isSpaceChar(c)) {
+			c = read();
 		}
+		int sgn = 1;
+		if (c == '-') {
+			sgn = -1;
+			c = read();
+		}
+		int res = 0;
+		do {
+			res *= 10;
+			res += c - '0';
+			c = read();
+		} while (!isSpaceChar(c));
+		return res * sgn;
+	}
+
+	public long nextLong() {
+		int c = read();
+		while (isSpaceChar(c)) {
+			c = read();
+		}
+		int sgn = 1;
+		if (c == '-') {
+			sgn = -1;
+			c = read();
+		}
+		long res = 0;
+		do {
+			res *= 10;
+			res += c - '0';
+			c = read();
+		} while (!isSpaceChar(c));
+		return res * sgn;
+	}
+
+	public int[] nextIntArray(int n) {
+		int a[] = new int[n];
+		for (int i = 0; i < n; i++) {
+			a[i] = nextInt();
+		}
+		return a;
+	}
+
+	public String nextLine() {
+		int c = read();
+		while (isSpaceChar(c))
+			c = read();
+		StringBuilder res = new StringBuilder();
+		do {
+			res.appendCodePoint(c);
+			c = read();
+		} while (!isEndOfLine(c));
+		return res.toString();
+	}
+
+	public boolean isSpaceChar(int c) {
+		return c == ' ' || c == '\n' || c == '\r' || c == '\t' || c == -1;
+	}
+
+	private boolean isEndOfLine(int c) {
+		return c == '\n' || c == '\r' || c == -1;
 	}
 }
