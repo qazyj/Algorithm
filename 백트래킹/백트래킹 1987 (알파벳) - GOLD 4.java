ece66aab9450b1730c3be67355e53ddbc0d37ba1
qazyj <qazyj@naver.com>
2021-02-10 22:31:51 +0900

    재귀식으로 돌렸던 백트래킹을 queue를 통해 비재귀식으로 바꾸었다. 메모리는 증가했지만 속도는 5배 빨라짐

--- 백트래킹/백트래킹 1987 (알파벳) - GOLD 4.java
+++ 백트래킹/백트래킹 1987 (알파벳) - GOLD 4.java
@@ -1,53 +1,154 @@
-import java.io.*;
-import java.util.*;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.InputMismatchException;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class Main {
+	static int R, C, answer, alphas[][];
+	static char[][] array;
+	static Queue<int[]> queue;
+	static boolean[] check;
+	static int[] x = { 0, 1, 0, -1 };
+	static int[] y = { 1, 0, -1, 0 };
 
-	static int R, C;
-	static int[][] array;
-	static boolean[] check = new boolean[26];
-	static int[] x = {0,1,0,-1};
-	static int[] y = {1,0,-1,0};
-	static int result = 0;
+	public static void main(String[] args) throws Exception {
+		SetData();
+		dfs();
+		System.out.println(answer);
+	}
 
-	public static void dfs(int a, int b, int count) {
-		if(check[array[a][b]]) {
-			result = Math.max(result, count);
-			return;
-		} else {
-			check[array[a][b]] = true;
-			for(int i = 0; i < 4; i++) {
-				int r = a + x[i];
-				int c = b + y[i];
+	private static void SetData() throws Exception {
+		InputReader in = new InputReader(System.in);
 
-				if(r >= 0 && c >= 0 && r < R && c < C) {
-					dfs(r, c, count + 1);
+		R = in.nextInt();
+		C = in.nextInt();
+		check = new boolean[26];
+		array = new char[R][C];
+		alphas = new int[R][C];
+		answer = 0;
+
+		for (int i = 0; i < R; i++)
+			array[i] = in.nextLine().toCharArray();
+
+		queue = new LinkedList<>();
+
+		queue.add(new int[] { 0, 0, 1 << (array[0][0] - 'a'), 1 });
+	}
+
+	public static void dfs() {
+		while (!queue.isEmpty()) {
+
+			int[] location = queue.poll();
+			if (alphas[location[0]][location[1]] == location[2])	continue;
+			alphas[location[0]][location[1]] = location[2];
+			answer = location[3];
+
+			for (int i = 0; i < 4; i++) {
+				int r = location[0] + x[i];
+				int c = location[1] + y[i];
+
+				if (r < 0 || c < 0 || r >= R || c >= C)		continue;
+
+				int next = 1 << (array[r][c] - 'a');
+
+				if ((location[2] & next) == 0) {
+					queue.add(new int[] { r, c, location[2] | next, location[3] + 1 });
 				}
 
 			}
-
-			check[array[a][b]] = false;
-
 		}
 	}
+}
 
-	public static void main(String[] args) throws IOException {
+class InputReader {
+	private final InputStream stream;
+	private final byte[] buf = new byte[8192];
+	private int curChar, snumChars;
 
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
+	public InputReader(InputStream st) {
+		this.stream = st;
+	}
 
-		R = Integer.parseInt(st.nextToken());
-		C = Integer.parseInt(st.nextToken());
-		array = new int[R][C];
-		for(int i = 0; i < R; i++) {
-			String s = br.readLine();
-			for(int j = 0; j < C; j++) {
-				array[i][j] = s.charAt(j) - 'A';
+	public int read() {
+		if (snumChars == -1)
+			throw new InputMismatchException();
+		if (curChar >= snumChars) {
+			curChar = 0;
+			try {
+				snumChars = stream.read(buf);
+			} catch (IOException e) {
+				throw new InputMismatchException();
 			}
+			if (snumChars <= 0)
+				return -1;
 		}
+		return buf[curChar++];
+	}
 
-		dfs(0, 0, 0);
+	public int nextInt() {
+		int c = read();
+		while (isSpaceChar(c)) {
+			c = read();
+		}
+		int sgn = 1;
+		if (c == '-') {
+			sgn = -1;
+			c = read();
+		}
+		int res = 0;
+		do {
+			res *= 10;
+			res += c - '0';
+			c = read();
+		} while (!isSpaceChar(c));
+		return res * sgn;
+	}
 
-		System.out.println(result);
+	public long nextLong() {
+		int c = read();
+		while (isSpaceChar(c)) {
+			c = read();
+		}
+		int sgn = 1;
+		if (c == '-') {
+			sgn = -1;
+			c = read();
+		}
+		long res = 0;
+		do {
+			res *= 10;
+			res += c - '0';
+			c = read();
+		} while (!isSpaceChar(c));
+		return res * sgn;
+	}
+
+	public int[] nextIntArray(int n) {
+		int a[] = new int[n];
+		for (int i = 0; i < n; i++) {
+			a[i] = nextInt();
+		}
+		return a;
+	}
+
+	public String nextLine() {
+		int c = read();
+		while (isSpaceChar(c))
+			c = read();
+		StringBuilder res = new StringBuilder();
+		do {
+			res.appendCodePoint(c);
+			c = read();
+		} while (!isEndOfLine(c));
+		return res.toString();
+	}
+
+	public boolean isSpaceChar(int c) {
+		return c == ' ' || c == '\n' || c == '\r' || c == '\t' || c == -1;
+	}
+
+	private boolean isEndOfLine(int c) {
+		return c == '\n' || c == '\r' || c == -1;
 	}
 }
