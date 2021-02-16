a2fe203920c9c2dcebb1e6d1d789e428f086d606
qazyj <qazyj@naver.com>
2021-02-16 22:33:03 +0900

    왼쪽 끝과 오른쪽 끝은 내려갈 수 있는 곳이 2곳으로 한정되어 있고, 중간은 세 곳 모두 갈 수 있으므로 연산을 최대값, 최소값만 찾아서 구해주면 답은 나온다.

--- DP/DP 2096 (내려가기) - GOLD 4.java
+++ DP/DP 2096 (내려가기) - GOLD 4.java
@@ -1,50 +1,134 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.util.InputMismatchException;
 
-public class Algorithm {
+public class Main {
 	static int N;
-	static int[][] dp;
+	static int[] min, max, array, temp;
 
 	public static void main(String[] args) throws Exception {
 		// TODO Auto-generated method stub
 		SetData();
-		System.out.print(dp[N-1][5] + " " + dp[N-1][0]);
+		System.out.println(Math.max(max[2], Math.max(max[0], max[1]))+" "+Math.min(min[2], Math.min(min[0], min[1])));
 	}
 
 	private static void SetData() throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = null;
+		InputReader in = new InputReader(System.in);
 		
-		N = Integer.parseInt(br.readLine());
-		int[] input = new int[3];
-		dp = new int[N][6];
-
-		st = new StringTokenizer(br.readLine());
-		for (int i = 0; i < 3; i++)
-			input[i] = Integer.parseInt(st.nextToken());
-
-		for (int i = 0; i < 3; i++)
-			dp[0][3 + i] = dp[0][i] = input[i];
-
-		for (int i = 1; i < N; i++) {
-			st = new StringTokenizer(br.readLine());
-
-			for (int j = 0; j < 3; j++)
-				input[j] = Integer.parseInt(st.nextToken());
-
-			int preIndex = i - 1;
-
-			dp[i][0] = Integer.min(dp[preIndex][0], dp[preIndex][1]) + input[0];
-			dp[i][1] = Integer.min(dp[preIndex][0], Math.min(dp[preIndex][1], dp[preIndex][2])) + input[1];
-			dp[i][2] = Integer.min(dp[preIndex][1], dp[preIndex][2]) + input[2];
-
-			dp[i][3] = Integer.max(dp[preIndex][3], dp[preIndex][4]) + input[0];
-			dp[i][4] = Integer.max(dp[preIndex][3], Math.max(dp[preIndex][4], dp[preIndex][5])) + input[1];
-			dp[i][5] = Integer.max(dp[preIndex][4], dp[preIndex][5]) + input[2];
+		N =  in.nextInt();
+		min = new int [3];
+		max = new int [3];
+		array = new int [3];
+		temp = new int [3];
+		
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < 3; j++) 
+				array[j] = in.nextInt();
+			temp[0] = array[0] + Math.min(min[0], min[1]);
+			temp[1] = array[1] + Math.min(min[2], Math.min(min[0], min[1]));
+			temp[2] = array[2] + Math.min(min[2], min[1]);
+			min[0] = temp[0];
+			min[1] = temp[1];
+			min[2] = temp[2];
+			temp[0] = array[0] + Math.max(max[0], max[1]);
+			temp[1] = array[1] + Math.max(max[2], Math.max(max[0], max[1]));
+			temp[2] = array[2] + Math.max(max[2], max[1]);
+			max[0] = temp[0];
+			max[1] = temp[1];
+			max[2] = temp[2];
 		}
+	}
+}
 
-		Arrays.sort(dp[N - 1]);
+class InputReader {
+	private final InputStream stream;
+	private final byte[] buf = new byte[8192];
+	private int curChar, snumChars;
+
+	public InputReader(InputStream st) {
+		this.stream = st;
+	}
+
+	public int read() {
+		if (snumChars == -1)
+			throw new InputMismatchException();
+		if (curChar >= snumChars) {
+			curChar = 0;
+			try {
+				snumChars = stream.read(buf);
+			} catch (IOException e) {
+				throw new InputMismatchException();
+			}
+			if (snumChars <= 0)
+				return -1;
+		}
+		return buf[curChar++];
+	}
+
+	public int nextInt() {
+		int c = read();
+		while (isSpaceChar(c)) {
+			c = read();
+		}
+		int sgn = 1;
+		if (c == '-') {
+			sgn = -1;
+			c = read();
+		}
+		int res = 0;
+		do {
+			res *= 10;
+			res += c - '0';
+			c = read();
+		} while (!isSpaceChar(c));
+		return res * sgn;
+	}
+
+	public long nextLong() {
+		int c = read();
+		while (isSpaceChar(c)) {
+			c = read();
+		}
+		int sgn = 1;
+		if (c == '-') {
+			sgn = -1;
+			c = read();
+		}
+		long res = 0;
+		do {
+			res *= 10;
+			res += c - '0';
+			c = read();
+		} while (!isSpaceChar(c));
+		return res * sgn;
+	}
+
+	public int[] nextIntArray(int n) {
+		int a[] = new int[n];
+		for (int i = 0; i < n; i++) {
+			a[i] = nextInt();
+		}
+		return a;
+	}
+
+	public String nextLine() {
+		int c = read();
+		while (isSpaceChar(c))
+			c = read();
+		StringBuilder res = new StringBuilder();
+		do {
+			res.appendCodePoint(c);
+			c = read();
+		} while (!isEndOfLine(c));
+		return res.toString();
+	}
+
+	public boolean isSpaceChar(int c) {
+		return c == ' ' || c == '\n' || c == '\r' || c == '\t' || c == -1;
+	}
+
+	private boolean isEndOfLine(int c) {
+		return c == '\n' || c == '\r' || c == -1;
 	}
 }
