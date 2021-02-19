fbe453a23004851aabcbf4de562f8905e7966cd1
qazyj <qazyj@naver.com>
2021-02-19 23:13:32 +0900

    반복문을 돌면서 1을 만나면 상, 하, 좌, 우의 값이 1인 index를 check해주면서 bfs를 돌렸다. check가 되지않았던 1을 처음 봤을 땐 count를 1씩 증가시켜 주면서 필요한 배추흰지렁이의 수를 맞춰주었다.

--- 깊이 우선 탐색/깊이 우선 탐색 1012 (유기농 배추) - SILVER 2.java
+++ 깊이 우선 탐색/깊이 우선 탐색 1012 (유기농 배추) - SILVER 2.java
@@ -1,10 +1,8 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.InputMismatchException;
 import java.util.LinkedList;
 import java.util.Queue;
-import java.util.StringTokenizer;
-
-// https://qazyj.tistory.com/71
 
 public class Main {
 	static int M, N;
@@ -13,29 +11,27 @@ public class Main {
 	static int[][] array;
 	static boolean[][] check;
 	static StringBuilder sb;
-	
+
 	public static void main(String[] args) throws Exception {
 		SetData();
 		System.out.println(sb);
 	}
 	
 	private static void SetData() throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		InputReader in = new InputReader(System.in);
 		
 		sb = new StringBuilder();
-		int testCase = Integer.parseInt(br.readLine());
+		int testCase = in.nextInt();
 
 		for (int i = 0; i < testCase; i++) {
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			M = Integer.parseInt(st.nextToken());
-			N = Integer.parseInt(st.nextToken());
-			int K = Integer.parseInt(st.nextToken());
+			M = in.nextInt();
+			N = in.nextInt();
+			int K = in.nextInt();
 			array = new int[M][N];
 			check = new boolean[M][N];
 			
 			for (int j = 0; j < K; j++) {
-				st = new StringTokenizer(br.readLine());
-				array[Integer.parseInt(st.nextToken())][Integer.parseInt(st.nextToken())] = 1;
+				array[in.nextInt()][in.nextInt()] = 1;
 			}
 
 			int count = 0;
@@ -72,5 +68,96 @@ public class Main {
 				}
 			}
 		}
+	}
+}
+class InputReader {
+	private final InputStream stream;
+	private final byte[] buf = new byte[8192];
+	private int curChar, snumChars;
+
+	public InputReader(InputStream st) {
+		this.stream = st;
+	}
+
+	public int read() {
+		if (snumChars == -1)
+			throw new InputMismatchException();
+		if (curChar >= snumChars) {
+			curChar = 0;
+			try {
+				snumChars = stream.read(buf);
+			} catch (IOException e) {
+				throw new InputMismatchException();
+			}
+			if (snumChars <= 0)
+				return -1;
+		}
+		return buf[curChar++];
+	}
+
+	public int nextInt() {
+		int c = read();
+		while (isSpaceChar(c)) {
+			c = read();
+		}
+		int sgn = 1;
+		if (c == '-') {
+			sgn = -1;
+			c = read();
+		}
+		int res = 0;
+		do {
+			res *= 10;
+			res += c - '0';
+			c = read();
+		} while (!isSpaceChar(c));
+		return res * sgn;
+	}
+
+	public long nextLong() {
+		int c = read();
+		while (isSpaceChar(c)) {
+			c = read();
+		}
+		int sgn = 1;
+		if (c == '-') {
+			sgn = -1;
+			c = read();
+		}
+		long res = 0;
+		do {
+			res *= 10;
+			res += c - '0';
+			c = read();
+		} while (!isSpaceChar(c));
+		return res * sgn;
+	}
+
+	public int[] nextIntArray(int n) {
+		int a[] = new int[n];
+		for (int i = 0; i < n; i++) {
+			a[i] = nextInt();
+		}
+		return a;
+	}
+
+	public String nextLine() {
+		int c = read();
+		while (isSpaceChar(c))
+			c = read();
+		StringBuilder res = new StringBuilder();
+		do {
+			res.appendCodePoint(c);
+			c = read();
+		} while (!isEndOfLine(c));
+		return res.toString();
+	}
+
+	public boolean isSpaceChar(int c) {
+		return c == ' ' || c == '\n' || c == '\r' || c == '\t' || c == -1;
+	}
+
+	private boolean isEndOfLine(int c) {
+		return c == '\n' || c == '\r' || c == -1;
 	}
 }
