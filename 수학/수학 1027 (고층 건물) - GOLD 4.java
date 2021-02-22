7ec83acc1ffa1ee4a717f9db7718000ba1adf629
qazyj <qazyj@naver.com>
2021-02-22 22:16:24 +0900

    기울기를 통해서 볼 수 있는 건물의 수를 파악한다. 가장 많이 보이는 건물의 최대값을 유지시켜줌.

--- 수학/수학 1027 (고층 건물) - GOLD 4.java
+++ 수학/수학 1027 (고층 건물) - GOLD 4.java
@@ -1,6 +1,8 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.InputMismatchException;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class Main {
 	static int answer, number;
@@ -11,19 +13,17 @@ public class Main {
 		Calculate();
 		System.out.println(answer);
 	}
-
+	
 	private static void SetData() throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		InputReader in = new InputReader(System.in);
 		
-		number = Integer.parseInt(br.readLine());
+		number = in.nextInt();
 		array = new long[number+1];
 		answer = 0;
 		
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int index=1;
-		while(st.hasMoreTokens()) {
-			array[index++] = Long.parseLong(st.nextToken());
-		}
+		for(int index = 1; index <= number; index++)
+			array[index] = in.nextLong();
+		
 	}
 	
 	private static void Calculate() {
@@ -50,5 +50,96 @@ public class Main {
 			
 			answer=Math.max(answer,temp);
 		}
+	}
+}
+class InputReader {
+	private final InputStream stream;
+	private final byte[] buf = new byte[8192];
+	private int curChar, snumChars;
+
+	public InputReader(InputStream st) {
+		this.stream = st;
+	}
+
+	public int read() {
+		if (snumChars == -1)
+			throw new InputMismatchException();
+		if (curChar >= snumChars) {
+			curChar = 0;
+			try {
+				snumChars = stream.read(buf);
+			} catch (IOException e) {
+				throw new InputMismatchException();
+			}
+			if (snumChars <= 0)
+				return -1;
+		}
+		return buf[curChar++];
+	}
+
+	public int nextInt() {
+		int c = read();
+		while (isSpaceChar(c)) {
+			c = read();
+		}
+		int sgn = 1;
+		if (c == '-') {
+			sgn = -1;
+			c = read();
+		}
+		int res = 0;
+		do {
+			res *= 10;
+			res += c - '0';
+			c = read();
+		} while (!isSpaceChar(c));
+		return res * sgn;
+	}
+
+	public long nextLong() {
+		int c = read();
+		while (isSpaceChar(c)) {
+			c = read();
+		}
+		int sgn = 1;
+		if (c == '-') {
+			sgn = -1;
+			c = read();
+		}
+		long res = 0;
+		do {
+			res *= 10;
+			res += c - '0';
+			c = read();
+		} while (!isSpaceChar(c));
+		return res * sgn;
+	}
+
+	public int[] nextIntArray(int n) {
+		int a[] = new int[n];
+		for (int i = 0; i < n; i++) {
+			a[i] = nextInt();
+		}
+		return a;
+	}
+
+	public String nextLine() {
+		int c = read();
+		while (isSpaceChar(c))
+			c = read();
+		StringBuilder res = new StringBuilder();
+		do {
+			res.appendCodePoint(c);
+			c = read();
+		} while (!isEndOfLine(c));
+		return res.toString();
+	}
+
+	public boolean isSpaceChar(int c) {
+		return c == ' ' || c == '\n' || c == '\r' || c == '\t' || c == -1;
+	}
+
+	private boolean isEndOfLine(int c) {
+		return c == '\n' || c == '\r' || c == -1;
 	}
 }
