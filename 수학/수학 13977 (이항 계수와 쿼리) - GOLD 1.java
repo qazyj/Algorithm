d7fa955cd96a37b6688a1436b9736b4ed2dc670b
qazyj <qazyj@naver.com>
2021-02-25 16:03:02 +0900

    미리 팩토리얼을 구한 뒤 역원까지 구해준 뒤 상수시간만에 해당 값을 도출

--- 수학/수학 13977 (이항 계수와 쿼리) - GOLD 1.java
+++ 수학/수학 13977 (이항 계수와 쿼리) - GOLD 1.java
@@ -1,58 +1,157 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.InputMismatchException;
+
 
 /* [페르마의 소정리] _ 혜민 코드 참고
 a^(p-1) % p = 1
 (a/b) % M = ((a % M) * (b^-1 % M)) = ((a % M) * (b^(M-2) % M))
 */
 
-public class Algorithm {
-	static StringBuilder sb;
-	static long[] fac,inv;
-	static final int MOD = 1000000007;
+public class Main {
+	private static  StringBuilder sb;
+    private static int MOD;
+    private static long[] F; // 8MB
 
 	public static void main(String[] args) throws Exception {
 		SetData();
 		System.out.println(sb);
 	}
-	
+
+	// 데이터
 	private static void SetData() throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = null;
+		InputReader in = new InputReader(System.in);
 		sb = new StringBuilder();
+		F = new long[4000001]; // 8MB
+		MOD = 1000000007;
 
-		fac = new long[4040404];
-		inv = new long[4040404];
-		fac[0] = 1;
-		for(int i=1; i<=4000000; i++) fac[i] = fac[i - 1] * i % MOD;
-		inv[4000000] = pow(fac[4000000], MOD-2);
-		for(int i=3999999; i>=0; i--){
-			inv[i] = inv[i+1] * (i + 1) % MOD;
-		}
-		
-		int M = Integer.parseInt(br.readLine());
-		for (int i = 0; i < M; i++) {
-			st = new StringTokenizer(br.readLine());
-			int N = Integer.parseInt(st.nextToken());
-			int K = Integer.parseInt(st.nextToken());
+        setFactorial();
 
-			long result = fac[N];
-			result = result * inv[K] % MOD;
-			result = result * inv[N-K] % MOD;
-			sb.append(String.valueOf(result) + "\n");
-		}
+        int T = in.nextInt();
+        while (T-- > 0) {
+            int N = in.nextInt();
+            int K = in.nextInt();
+            sb.append(Combination(N, K)).append('\n');
+        }
 	}
-	private static long pow(long a, int b) {
-		long result = 1;
 
-		while (b > 0) {
-			if (b % 2 == 1)
-				result = result * a % MOD;
-			b >>= 1;
-			a = (a * a) % MOD;
+    private static long Combination(int N, int K) {
+        long numer = F[N];
+        long denom = (F[N - K] * F[K]) % MOD;
+        denom = Pow(denom, MOD - 2);
+        
+        return (numer * denom) % MOD;
+    }
+
+    private static long Pow(long n, int k) {
+        long base = 1;
+        while (k > 0) {
+            if (k % 2 == 1) { base *= n; base %= MOD; }
+            n *= n; n %= MOD;
+            k /= 2;
+        }
+
+        return base;
+    }
+
+    private static void setFactorial() {
+        F[0] = 1; F[1] = 1;
+        for (int i = 2; i < 4000001; i++) {
+            F[i] = i * F[i - 1];
+            F[i] %= MOD;
+        }
+    }
+}
+	
+
+class InputReader {
+	private final InputStream stream;
+	private final byte[] buf = new byte[8192];
+	private int curChar, snumChars;
+
+	public InputReader(InputStream st) {
+		this.stream = st;
+	}
+
+	public int read() {
+		if (snumChars == -1)
+			throw new InputMismatchException();
+		if (curChar >= snumChars) {
+			curChar = 0;
+			try {
+				snumChars = stream.read(buf);
+			} catch (IOException e) {
+				throw new InputMismatchException();
+			}
+			if (snumChars <= 0)
+				return -1;
 		}
+		return buf[curChar++];
+	}
 
-		return result;
+	public int nextInt() {
+		int c = read();
+		while (isSpaceChar(c)) {
+			c = read();
+		}
+		int sgn = 1;
+		if (c == '-') {
+			sgn = -1;
+			c = read();
+		}
+		int res = 0;
+		do {
+			res *= 10;
+			res += c - '0';
+			c = read();
+		} while (!isSpaceChar(c));
+		return res * sgn;
+	}
+
+	public long nextLong() {
+		int c = read();
+		while (isSpaceChar(c)) {
+			c = read();
+		}
+		int sgn = 1;
+		if (c == '-') {
+			sgn = -1;
+			c = read();
+		}
+		long res = 0;
+		do {
+			res *= 10;
+			res += c - '0';
+			c = read();
+		} while (!isSpaceChar(c));
+		return res * sgn;
+	}
+
+	public int[] nextIntArray(int n) {
+		int a[] = new int[n];
+		for (int i = 0; i < n; i++) {
+			a[i] = nextInt();
+		}
+		return a;
+	}
+
+	public String nextLine() {
+		int c = read();
+		while (isSpaceChar(c))
+			c = read();
+		StringBuilder res = new StringBuilder();
+		do {
+			res.appendCodePoint(c);
+			c = read();
+		} while (!isEndOfLine(c));
+		return res.toString();
+	}
+
+	public boolean isSpaceChar(int c) {
+		return c == ' ' || c == '\n' || c == '\r' || c == '\t' || c == -1;
+	}
+
+	private boolean isEndOfLine(int c) {
+		return c == '\n' || c == '\r' || c == -1;
 	}
 }
