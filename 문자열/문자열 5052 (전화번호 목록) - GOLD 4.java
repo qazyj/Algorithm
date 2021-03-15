4199418c08deeaa729bccec0be9ddbebce0d521e
qazyj <qazyj@naver.com>
2021-03-15 20:28:20 +0900

    정렬과 startsWith를 이용하면 쉽게 풀 수 있다.

---
+++ 문자열/문자열 5052 (전화번호 목록) - GOLD 4.java
@@ -0,0 +1,146 @@
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.InputMismatchException;
+
+public class Algorithm {
+	static StringBuilder sb;
+	static int N, L, answer;
+
+	public static void main(String[] args) throws Exception {
+		SetData();
+		System.out.println(sb);
+	}
+
+	// 데이터
+	private static void SetData() throws Exception {
+		InputReader in = new InputReader(System.in);
+
+		int testcase = in.nextInt();
+		sb = new StringBuilder();
+		int n;
+		boolean check;
+		
+		for(int i=0;i<testcase;i++)
+		{
+			check = false;
+			n = in.nextInt();
+			String[] array = new String[n];
+            for(int j=0;j<n;j++) {
+                array[j]=in.nextLine();
+            }
+            
+            
+            Arrays.sort(array, new Comparator<String>() {
+                @Override
+                public int compare(String o1, String o2) {
+                    return o1.compareTo(o2);                    
+                }
+            });
+            
+            for(int j=1;j<n;j++) {
+                if(array[j].startsWith(array[j-1])) {
+                    check=true;
+                    break;
+                }
+            }
+            
+            if(check) sb.append("NO").append("\n");
+            else sb.append("YES").append("\n");
+		}		
+	}
+	
+}
+
+class InputReader {
+	private final InputStream stream;
+	private final byte[] buf = new byte[8192];
+	private int curChar, snumChars;
+
+	public InputReader(InputStream st) {
+		this.stream = st;
+	}
+
+	public int read() {
+		if (snumChars == -1)
+			throw new InputMismatchException();
+		if (curChar >= snumChars) {
+			curChar = 0;
+			try {
+				snumChars = stream.read(buf);
+			} catch (IOException e) {
+				throw new InputMismatchException();
+			}
+			if (snumChars <= 0)
+				return -1;
+		}
+		return buf[curChar++];
+	}
+
+	public int nextInt() {
+		int c = read();
+		while (isSpaceChar(c)) {
+			c = read();
+		}
+		int sgn = 1;
+		if (c == '-') {
+			sgn = -1;
+			c = read();
+		}
+		int res = 0;
+		do {
+			res *= 10;
+			res += c - '0';
+			c = read();
+		} while (!isSpaceChar(c));
+		return res * sgn;
+	}
+
+	public long nextLong() {
+		int c = read();
+		while (isSpaceChar(c)) {
+			c = read();
+		}
+		int sgn = 1;
+		if (c == '-') {
+			sgn = -1;
+			c = read();
+		}
+		long res = 0;
+		do {
+			res *= 10;
+			res += c - '0';
+			c = read();
+		} while (!isSpaceChar(c));
+		return res * sgn;
+	}
+
+	public int[] nextIntArray(int n) {
+		int a[] = new int[n];
+		for (int i = 0; i < n; i++) {
+			a[i] = nextInt();
+		}
+		return a;
+	}
+
+	public String nextLine() {
+		int c = read();
+		while (isSpaceChar(c))
+			c = read();
+		StringBuilder res = new StringBuilder();
+		do {
+			res.appendCodePoint(c);
+			c = read();
+		} while (!isEndOfLine(c));
+		return res.toString();
+	}
+
+	public boolean isSpaceChar(int c) {
+		return c == ' ' || c == '\n' || c == '\r' || c == '\t' || c == -1;
+	}
+
+	private boolean isEndOfLine(int c) {
+		return c == '\n' || c == '\r' || c == -1;
+	}
+}
