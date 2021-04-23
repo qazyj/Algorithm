6cd08b64f2f704b5207aa35aa9694802462744f7
qazyj <qazyj@naver.com>
2021-04-23 19:54:19 +0900

    점화식 찾고 구현

---
+++ 백준/DP/DP 2133 (타일 채우기) - SILVER 1.java
@@ -0,0 +1,125 @@
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.InputMismatchException;
+
+public class Algorithm {
+	static int N;
+	static long[] dp;
+
+	public static void main(String[] args) throws Exception {
+		SetData();
+		System.out.println(dp[N]);
+	}
+
+	// 데이터
+	private static void SetData() throws Exception {
+		InputReader in = new InputReader(System.in);
+
+		N = in.nextInt();
+		dp = new long[31];
+		dp[0] = 1;
+		dp[1] = 0;
+		if(N>1)
+			dp[2] = 3;
+		
+		for(int i=4;i<=N;i+=2) { 
+			dp[i] = 3*dp[i-2]; 
+			for(int j=0;j<i-2;j+=2) { 
+				dp[i]+=dp[j] * 2; 
+			}
+		}
+		
+	}
+}
+
+class InputReader {
+	private final InputStream stream;
+	private final byte[] buf = new byte[8192];
+	private int curChar, snumChars;
+
+	public InputReader(InputStream st) {
+		this.stream = st;
+	}
+
+	public int read() {
+		if (snumChars == -1)
+			throw new InputMismatchException();
+		if (curChar >= snumChars) {
+			curChar = 0;
+			try {
+				snumChars = stream.read(buf);
+			} catch (IOException e) {
+				throw new InputMismatchException();
+			}
+			if (snumChars <= 0)
+				return -1;
+		}
+		return buf[curChar++];
+	}
+
+	public int nextInt() {
+		int c = read();
+		while (isSpaceChar(c)) {
+			c = read();
+		}
+		int sgn = 1;
+		if (c == '-') {
+			sgn = -1;
+			c = read();
+		}
+		int res = 0;
+		do {
+			res *= 10;
+			res += c - '0';
+			c = read();
+		} while (!isSpaceChar(c));
+		return res * sgn;
+	}
+
+	public long nextLong() {
+		int c = read();
+		while (isSpaceChar(c)) {
+			c = read();
+		}
+		int sgn = 1;
+		if (c == '-') {
+			sgn = -1;
+			c = read();
+		}
+		long res = 0;
+		do {
+			res *= 10;
+			res += c - '0';
+			c = read();
+		} while (!isSpaceChar(c));
+		return res * sgn;
+	}
+
+	public int[] nextIntArray(int n) {
+		int a[] = new int[n];
+		for (int i = 0; i < n; i++) {
+			a[i] = nextInt();
+		}
+		return a;
+	}
+
+	public String nextLine() {
+		int c = read();
+		while (isSpaceChar(c))
+			c = read();
+		StringBuilder res = new StringBuilder();
+		do {
+			res.appendCodePoint(c);
+			c = read();
+		} while (!isEndOfLine(c));
+		return res.toString();
+	}
+
+	public boolean isSpaceChar(int c) {
+		return c == ' ' || c == '\n' || c == '\r' || c == '\t' || c == -1;
+	}
+
+	private boolean isEndOfLine(int c) {
+		return c == '\n' || c == '\r' || c == -1;
+	}
+}
