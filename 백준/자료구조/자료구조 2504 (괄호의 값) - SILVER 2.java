39aa391a8b5a8726276b967f49df4be89743f70e
DESKTOP-SURF07L\Kim & An <qazyj@naver.com>
2021-08-02 17:17:14 +0900

    스택 사용

---
+++ 백준/자료구조/자료구조 2504 (괄호의 값) - SILVER 2.java
@@ -0,0 +1,175 @@
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.InputMismatchException;
+import java.util.Stack;
+
+public class Main {
+	static String s;
+	static int answer;
+
+	public static void main(String[] args) throws Exception {
+		SetData();
+		System.out.println(answer);
+	}
+
+	// 데이터
+	private static void SetData() throws Exception {
+		InputReader in = new InputReader(System.in);
+
+		s = in.nextLine();
+		answer = 0;
+		SaveAnswer();		
+	}
+	
+	private static void SaveAnswer() {
+		Stack<Integer> stack=new Stack<>();
+		int temp=1;
+		for (int i = 0; i < s.length(); i++) {
+			if(s.charAt(i)=='(') {
+				stack.add(2);
+				temp*=2;
+			}else if(s.charAt(i)=='[') {
+				stack.add(3);
+				temp*=3;
+			}else {
+				// 여는 괄호 X
+				if(stack.size()==0) {
+					answer=0;
+					break;
+				}
+				
+				// 여는 괄호 X && 닫는 괄호 X
+				if(s.charAt(i)!=']' && s.charAt(i)!=')') {
+					answer=0;
+					break;
+				}
+				
+				// 괄호 매칭 X
+				if((s.charAt(i)==']' && stack.peek()==2) || (s.charAt(i)==')' && stack.peek()==3)) {
+					answer=0;
+					break;
+				}
+
+				if((s.charAt(i-1)=='(' && s.charAt(i) == ')') || (s.charAt(i-1)=='[' && s.charAt(i) == ']')) {
+					answer+=temp;
+				}
+				temp/=stack.pop();
+			}
+			
+		}
+		
+		if(stack.size()!=0) {
+			answer=0;
+		}
+	}
+}
+
+class Node implements Comparable<Node> {
+	long age;
+	int index;
+
+	public Node(long age, int index) {
+		this.age= age;
+		this.index = index;
+	}
+
+	@Override
+	public int compareTo(Node o) {
+		// TODO Auto-generated method stub
+		return Long.compare(this.age, o.age);
+	}
+}
+
+class InputReader {
+	private final InputStream stream;
+	private final byte[] buf = new byte[8192];
+	private int curChar, snumChars;
+
+	public InputReader(InputStream st) {
+		this.stream = st;
+	}
+
+	public int read() {
+		if (snumChars == -1)
+			throw new InputMismatchException();
+		if (curChar >= snumChars) {
+			curChar = 0;
+			try {
+				snumChars = stream.read(buf);
+			} catch (IOException e) {
+				throw new InputMismatchException();
+			}
+			if (snumChars <= 0)
+				return -1;
+		}
+		return buf[curChar++];
+	}
+
+	public int nextInt() {
+		int c = read();
+		while (isSpaceChar(c)) {
+			c = read();
+		}
+		int sgn = 1;
+		if (c == '-') {
+			sgn = -1;
+			c = read();
+		}
+		int res = 0;
+		do {
+			res *= 10;
+			res += c - '0';
+			c = read();
+		} while (!isSpaceChar(c));
+		return res * sgn;
+	}
+
+	public long nextLong() {
+		int c = read();
+		while (isSpaceChar(c)) {
+			c = read();
+		}
+		int sgn = 1;
+		if (c == '-') {
+			sgn = -1;
+			c = read();
+		}
+		long res = 0;
+		do {
+			res *= 10;
+			res += c - '0';
+			c = read();
+		} while (!isSpaceChar(c));
+		return res * sgn;
+	}
+
+	public int[] nextIntArray(int n) {
+		int a[] = new int[n];
+		for (int i = 0; i < n; i++) {
+			a[i] = nextInt();
+		}
+		return a;
+	}
+
+	public String nextLine() {
+		int c = read();
+		while (isSpaceChar(c))
+			c = read();
+		StringBuilder res = new StringBuilder();
+		do {
+			res.appendCodePoint(c);
+			c = read();
+		} while (!isEndOfLine(c));
+		return res.toString();
+	}
+
+	public boolean isSpaceChar(int c) {
+		return c == ' ' || c == '\n' || c == '\r' || c == '\t' || c == -1;
+	}
+
+	private boolean isEndOfLine(int c) {
+		return c == '\n' || c == '\r' || c == -1;
+	}
+}
