105c632265b283e287e8aa6873458c33b414dee1
DESKTOP-SURF07L\Kim & An <qazyj@naver.com>
2021-08-10 15:01:23 +0900

    문제 설명대로 bfs방식으로 구현

--- 백준/너비 우선 탐색/너비 우선 탐색 1963 (소수 경로) - GOLD 4.java
+++ 백준/너비 우선 탐색/너비 우선 탐색 1963 (소수 경로) - GOLD 4.java
@@ -5,8 +5,8 @@ import java.util.InputMismatchException;
 import java.util.LinkedList;
 import java.util.Queue;
 
-public class Algorithm {
-	static boolean[] primes;
+public class Main {
+	static boolean[] primes, check;
 	static Queue<int[]> queue;
 	static StringBuilder sb;
 
@@ -23,9 +23,11 @@ public class Algorithm {
 
 		int testcase = in.nextInt();
 		primes = new boolean[10000];
-		
-		for (int i = 0; i < testcase; i++) {
-			Arrays.fill(primes, false);
+		check = new boolean[10000];
+		getPrimeNumber();
+
+		for (int t = 0; t < testcase; t++) {
+			Arrays.fill(check, false);
 			bfs(in.nextInt(), in.nextInt());
 		}
 	}
@@ -37,43 +39,58 @@ public class Algorithm {
 			sb.append(0).append("\n");
 			return;
 		}
+		
 		while (!queue.isEmpty()) {
 			int[] primeRoot = queue.poll();
-			if (!primes[primeRoot[0]]) {
-				// 지나온건 다시 안돌아가도록
-				primes[primeRoot[0]] = true;
-				
-				char[] numbers = Integer.toString(primeRoot[0]).toCharArray();
+			if (check[primeRoot[0]])	continue;
 
-				for (int i = 0; i <= 9; i++) {
-					for (int j = 0; j < 4; j++) {
-						if (j == 0 && i == 0)   continue;
-						
-						char temp = numbers[j];
-						numbers[j] = (char) (i + '0');
-						int changedPrimeNumber = Integer.parseInt(String.valueOf(numbers));
-						numbers[j] = temp;
-						if (primes[changedPrimeNumber])		continue;
-						if (check(changedPrimeNumber)) {
-							if (changedPrimeNumber == postNumber) {
-								sb.append(primeRoot[1] + 1).append("\n");
-								return;
-							}
-							queue.add(new int[] { changedPrimeNumber, primeRoot[1] + 1 });
+			// 지나온건 다시 안돌아가도록 check
+			check[primeRoot[0]] = true;
+
+			char[] numbers = Integer.toString(primeRoot[0]).toCharArray();
+
+			// 4자리 모두 0~9로 변경
+			for (int i = 0; i <= 9; i++) {
+				for (int j = 0; j < 4; j++) {
+					// 맨 앞이 0이 되는 경우 제외
+					if (j == 0 && i == 0)	continue;
+
+					// 숫자 변경
+					char temp = numbers[j];
+					numbers[j] = (char) (i + '0');
+					int changedPrimeNumber = Integer.parseInt(String.valueOf(numbers));
+					numbers[j] = temp;
+
+					// 바꾼 값이 이미 간적이 있으면
+					if (check[changedPrimeNumber])		continue;
+					
+					if (!primes[changedPrimeNumber]) {
+						if (changedPrimeNumber == postNumber) {
+							sb.append(primeRoot[1] + 1).append("\n");
+							return;
 						}
+						queue.add(new int[] { changedPrimeNumber, primeRoot[1] + 1 });
 					}
 				}
 			}
 		}
+		
 		sb.append("Impossible").append("\n");
 	}
 
-	private static boolean check(int number) {
-		for (int i = 2; i <= (int) Math.sqrt(number); i++) {
-			if (number % i == 0)
-				return false;
+	private static void getPrimeNumber() {
+		primes[0] = primes[1] = true;
+		for (int i = 2; i < 10000; i++) {
+			// true면 이미 소수이므로 pass
+			if (primes[i]) {
+				continue;
+			}
+
+			// 해당 수로 나누어 떨어지는 수는 소수이므로 true로 check
+			for (int j = i + i; j < 10000; j += i) {
+				primes[j] = true;
+			}
 		}
-		return true;
 	}
 }
 
