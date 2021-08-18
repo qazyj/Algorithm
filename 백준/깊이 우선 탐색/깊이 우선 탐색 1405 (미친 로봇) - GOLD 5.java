78b5aec254a6d5c6ab5902cadc4373c6556faeb6
DESKTOP-SURF07L\Kim & An <qazyj@naver.com>
2021-08-18 15:23:38 +0900

    dfs 사용

--- 백준/깊이 우선 탐색/깊이 우선 탐색 1405 (미친 로봇) - GOLD 5.java
+++ 백준/깊이 우선 탐색/깊이 우선 탐색 1405 (미친 로봇) - GOLD 5.java
@@ -1,19 +1,19 @@
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Arrays;
 import java.util.InputMismatchException;
 
-public class Algorithm {
+public class Main {
 	static int N;
-	static double percentage;	
-	static double[] percent;	// 각 방향으로 이동할 확률
-	static boolean[][] check;	// 단순 경로인지 아닌지 체크
+	static double answer;	
+	static double[] percent;	
+	static boolean[][] check;	
 	static int[] x = { -1, 1, 0, 0 };
 	static int[] y = { 0, 0, -1, 1 };
 
 	public static void main(String[] args) throws Exception {
 		SetData();
-		dfs(14, 14, 0, 1.0);
-		System.out.println(percentage);
+		System.out.println(answer);
 	}
 
 	// 데이터
@@ -21,97 +21,125 @@ public class Algorithm {
 		InputReader in = new InputReader(System.in);
 
 		percent = new double[4];
-		check = new boolean[30][30];
-		percentage = 0.0;
-		check[14][14] = true;	
+		check = new boolean[29][29];
+		answer = 0.0;
 
-		N = in.readInt();		
+		N = in.nextInt();
 
-		// 입력값을 확률로 바꿈
 		for (int i = 0; i < 4; i++) 
-			percent[i] = in.readInt() * 0.01;
+			percent[i] = in.nextInt() * 0.01;
 		
+		check[14][14] = true;	
+		dfs(14, 14, 0, 1.0);
 	}
 
-	private static void dfs(int a, int b, int count, double per) {
-		// basecase
+	private static void dfs(int a, int b, int count, double percentage) {
 		if (count == N) {
-			percentage += per;
+			answer += percentage;
 			return;
 		}
 
-		// 4방향 이동
-		for (int i = 0; i < 4; i++) {
-			int r = a + x[i];
-			int c = b + y[i];
-			if (!check[r][c]) {
-				check[r][c] = true;
-				dfs(r, c, count + 1, per * percent[i]);
-				check[r][c] = false;
-			}
+		for (int direction = 0; direction < 4; direction++) {
+			int r = a + x[direction];
+			int c = b + y[direction];
+			
+			if (check[r][c])  continue;
+			
+			check[r][c] = true;
+			dfs(r, c, count + 1, percentage * percent[direction]);
+			check[r][c] = false;
 		}
 	}
-	
-	private static class InputReader {
-		private InputStream stream;
-		private byte[] buf = new byte[1024];
-		private int curChar;
-		private int numChars;
-		private SpaceCharFilter filter;
+}
 
-		public InputReader(InputStream stream) {
-			this.stream = stream;
-		}
+class InputReader {
+	private final InputStream stream;
+	private final byte[] buf = new byte[8192];
+	private int curChar, snumChars;
 
-		public int read() {
-			if (numChars == -1) {
+	public InputReader(InputStream st) {
+		this.stream = st;
+	}
+
+	public int read() {
+		if (snumChars == -1)
+			throw new InputMismatchException();
+		if (curChar >= snumChars) {
+			curChar = 0;
+			try {
+				snumChars = stream.read(buf);
+			} catch (IOException e) {
 				throw new InputMismatchException();
 			}
-			if (curChar >= numChars) {
-				curChar = 0;
-				try {
-					numChars = stream.read(buf);
-				} catch (IOException e) {
-					throw new InputMismatchException();
-				}
-				if (numChars <= 0) {
-					return -1;
-				}
-			}
-			return buf[curChar++];
+			if (snumChars <= 0)
+				return -1;
 		}
+		return buf[curChar++];
+	}
 
-		public int readInt() {
-			int c = read();
-			while (isSpaceChar(c)) {
-				c = read();
-			}
-			int sgn = 1;
-			if (c == '-') {
-				sgn = -1;
-				c = read();
-			}
-			int res = 0;
-			do {
-				if (c < '0' || c > '9') {
-					throw new InputMismatchException();
-				}
-				res *= 10;
-				res += c - '0';
-				c = read();
-			} while (!isSpaceChar(c));
-			return res * sgn;
+	public int nextInt() {
+		int c = read();
+		while (isSpaceChar(c)) {
+			c = read();
 		}
+		int sgn = 1;
+		if (c == '-') {
+			sgn = -1;
+			c = read();
+		}
+		int res = 0;
+		do {
+			res *= 10;
+			res += c - '0';
+			c = read();
+		} while (!isSpaceChar(c));
+		return res * sgn;
+	}
 
-		public boolean isSpaceChar(int c) {
-			if (filter != null) {
-				return filter.isSpaceChar(c);
-			}
-			return c == ' ' || c == '\n' || c == '\r' || c == '\t' || c == -1;
+	public long nextLong() {
+		int c = read();
+		while (isSpaceChar(c)) {
+			c = read();
 		}
+		int sgn = 1;
+		if (c == '-') {
+			sgn = -1;
+			c = read();
+		}
+		long res = 0;
+		do {
+			res *= 10;
+			res += c - '0';
+			c = read();
+		} while (!isSpaceChar(c));
+		return res * sgn;
+	}
 
-		public interface SpaceCharFilter {
-			public boolean isSpaceChar(int ch);
+	public int[] nextIntArray(int n) {
+		int a[] = new int[n];
+		for (int i = 0; i < n; i++) {
+			a[i] = nextInt();
 		}
+		return a;
+	}
+
+	public String nextLine() {
+		int c = read();
+		while (isSpaceChar(c))
+			c = read();
+		StringBuilder res = new StringBuilder();
+		do {
+			res.appendCodePoint(c);
+			c = read();
+		} while (!isEndOfLine(c));
+		return res.toString();
+	}
+
+	public boolean isSpaceChar(int c) {
+		return c == ' ' || c == '\n' || c == '\r' || c == '\t' || c == -1;
+	}
+
+	private boolean isEndOfLine(int c) {
+		return c == '\n' || c == '\r' || c == -1;
 	}
 }
