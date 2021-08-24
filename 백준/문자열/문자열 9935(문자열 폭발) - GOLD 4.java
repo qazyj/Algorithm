95e53e8fedc00f4f8b3ff50f5de0cb99c80a865f
DESKTOP-SURF07L\Kim & An <qazyj@naver.com>
2021-08-24 15:35:46 +0900

    스택 사용

--- 백준/문자열/문자열 9935(문자열 폭발) - GOLD 4.java
+++ 백준/문자열/문자열 9935(문자열 폭발) - GOLD 4.java
@@ -1,20 +1,30 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.InputMismatchException;
+import java.util.Iterator;
 import java.util.Stack;
-import java.util.StringTokenizer;
 
 public class Main {
-	
-	public static void main(String[] args) throws IOException {
-		// TODO Auto-generated method stub        
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static StringBuilder sb;
+
+	public static void main(String[] args) throws Exception {
+		SetData();
+		System.out.println(sb);
+	}
+
+	// 데이터
+	private static void SetData() throws Exception {
+		InputReader in = new InputReader(System.in);
+		
+        String BombedString = in.nextLine();
+        String bombString = in.nextLine();
+        sb = new StringBuilder();
         Stack<Character> stack = new Stack<Character> ();
-        
-        String BombedString = br.readLine();
-        String bombString = br.readLine(); 
 
         for(int i = BombedString.length() - 1; i >= 0;  i--){
             stack.push(BombedString.charAt(i));   
@@ -37,16 +47,122 @@ public class Main {
         
 
         int stackSize = stack.size();
-        StringBuilder sb = new StringBuilder();
         
         if(stack.isEmpty()){
-               System.out.println("FRULA");
+               sb.append("FRULA");
         }else{
                for(int i = 0; i < stackSize; i++) sb.append(stack.pop());
         }
+	}
 
-        System.out.println(sb);
+}
+/*
+class Node implements Comparable<Node> {
+	int number;
+	double fail;
+	
+	public Node(int number, double fail) {
+		this.number = number;
+		this.fail = fail;
+	}
 
-    }
+	@Override
+	public int compareTo(Node o) {
+		// TODO Auto-generated method stub
+		if(this.fail == o.fail)
+			return Integer.compare(this.number, o.number);
+		return -Double.compare(this.fail, o.fail);
+	}
+}*/
 
+class InputReader {
+	private final InputStream stream;
+	private final byte[] buf = new byte[8192];
+	private int curChar, snumChars;
+
+	public InputReader(InputStream st) {
+		this.stream = st;
+	}
+
+	public int read() {
+		if (snumChars == -1)
+			throw new InputMismatchException();
+		if (curChar >= snumChars) {
+			curChar = 0;
+			try {
+				snumChars = stream.read(buf);
+			} catch (IOException e) {
+				throw new InputMismatchException();
+			}
+			if (snumChars <= 0)
+				return -1;
+		}
+		return buf[curChar++];
+	}
+
+	public int nextInt() {
+		int c = read();
+		while (isSpaceChar(c)) {
+			c = read();
+		}
+		int sgn = 1;
+		if (c == '-') {
+			sgn = -1;
+			c = read();
+		}
+		int res = 0;
+		do {
+			res *= 10;
+			res += c - '0';
+			c = read();
+		} while (!isSpaceChar(c));
+		return res * sgn;
+	}
+
+	public long nextLong() {
+		int c = read();
+		while (isSpaceChar(c)) {
+			c = read();
+		}
+		int sgn = 1;
+		if (c == '-') {
+			sgn = -1;
+			c = read();
+		}
+		long res = 0;
+		do {
+			res *= 10;
+			res += c - '0';
+			c = read();
+		} while (!isSpaceChar(c));
+		return res * sgn;
+	}
+
+	public int[] nextIntArray(int n) {
+		int a[] = new int[n];
+		for (int i = 0; i < n; i++) {
+			a[i] = nextInt();
+		}
+		return a;
+	}
+
+	public String nextLine() {
+		int c = read();
+		while (isSpaceChar(c))
+			c = read();
+		StringBuilder res = new StringBuilder();
+		do {
+			res.appendCodePoint(c);
+			c = read();
+		} while (!isEndOfLine(c));
+		return res.toString();
+	}
+
+	public boolean isSpaceChar(int c) {
+		return c == ' ' || c == '\n' || c == '\r' || c == '\t' || c == -1;
+	}
+
+	private boolean isEndOfLine(int c) {
+		return c == '\n' || c == '\r' || c == -1;
+	}
 }
