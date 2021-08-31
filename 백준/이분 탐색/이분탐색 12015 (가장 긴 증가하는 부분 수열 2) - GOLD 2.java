44bf913204cd36fb649769eb26e912017864d60f
DESKTOP-SURF07L\Kim & An <qazyj@naver.com>
2021-08-31 16:21:52 +0900

    가장 긴 증가하는 부분 수열 + 이분 탐색

--- 백준/이분 탐색/이분탐색 12015 (가장 긴 증가하는 부분 수열 2) - GOLD 2.java
+++ 백준/이분 탐색/이분탐색 12015 (가장 긴 증가하는 부분 수열 2) - GOLD 2.java
@@ -1,44 +1,153 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Set;
+import java.util.HashSet;
+import java.util.InputMismatchException;
+import java.util.LinkedList;
+import java.util.PriorityQueue;
+import java.util.Queue;
 
-public class Algorithm {
-
+public class Main {
+	static int N, answer;
+	static int[] inputArray, LIS;
+	
 	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		SetData();
+		System.out.println(answer);
+	}
 
-		long N = Integer.parseInt(br.readLine());
-		long[] inputArray = new long[(int) N];
-		long[] LIS = new long[(int) N];
+	// 데이터
+	private static void SetData() throws Exception {
+		InputReader in = new InputReader(System.in);
+		
+		N = in.nextInt();
+		inputArray = new int[N];
+		LIS = new int[N];
+		answer = 0;
 
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		for (long i = 0; i < N; i++) {
-			inputArray[(int) i] = Integer.parseInt(st.nextToken());
+		for (int i = 0; i < N; i++) {
+			inputArray[i] = in.nextInt();
 		}
 
 		LIS[0] = inputArray[0];
-		long j = 0;
+		int j = 0;
 
-		for (long i = 1; i < N; i++) {
-			if (LIS[(int) j] < inputArray[(int) i]) {
-				LIS[(int) ++j] = inputArray[(int) i];
+		for (int i = 1; i < N; i++) {
+			if (LIS[j] < inputArray[i]) {
+				LIS[++j] = inputArray[i];
 			} else {
-				long ans = binarySearch(LIS, 0, j, inputArray[(int) i]);
-				LIS[(int) ans] = Math.min(LIS[(int) ans], inputArray[(int) i]);
+				int temp = binarySearch(0, j, inputArray[i]);
+				LIS[temp] = Math.min(LIS[temp], inputArray[i]);
 			}
 		}
-		System.out.println(j + 1);
+
+		answer = j + 1;
 	}
 
-	static long binarySearch(long[] inputArray, int startIndex, long endIndex, long key) {
+	static int binarySearch(int startIndex, int endIndex, int key) {
 		while (startIndex <= endIndex) {
-			int mid = (int) ((startIndex + endIndex) / 2); 
-			if (inputArray[mid] < key) {
+			int mid = (startIndex + endIndex) / 2;
+			if (LIS[mid] < key) {
 				startIndex = mid + 1;
 			} else {
 				endIndex = mid - 1;
 			}
 		}
 		return startIndex;
+	}
+}
+
+class InputReader {
+	private final InputStream stream;
+	private final byte[] buf = new byte[8192];
+	private int curChar, snumChars;
+
+	public InputReader(InputStream st) {
+		this.stream = st;
+	}
+
+	public int read() {
+		if (snumChars == -1)
+			throw new InputMismatchException();
+		if (curChar >= snumChars) {
+			curChar = 0;
+			try {
+				snumChars = stream.read(buf);
+			} catch (IOException e) {
+				throw new InputMismatchException();
+			}
+			if (snumChars <= 0)
+				return -1;
+		}
+		return buf[curChar++];
+	}
+
+	public int nextInt() {
+		int c = read();
+		while (isSpaceChar(c)) {
+			c = read();
+		}
+		int sgn = 1;
+		if (c == '-') {
+			sgn = -1;
+			c = read();
+		}
+		int res = 0;
+		do {
+			res *= 10;
+			res += c - '0';
+			c = read();
+		} while (!isSpaceChar(c));
+		return res * sgn;
+	}
+
+	public long nextLong() {
+		int c = read();
+		while (isSpaceChar(c)) {
+			c = read();
+		}
+		int sgn = 1;
+		if (c == '-') {
+			sgn = -1;
+			c = read();
+		}
+		long res = 0;
+		do {
+			res *= 10;
+			res += c - '0';
+			c = read();
+		} while (!isSpaceChar(c));
+		return res * sgn;
+	}
+
+	public int[] nextIntArray(int n) {
+		int a[] = new int[n];
+		for (int i = 0; i < n; i++) {
+			a[i] = nextInt();
+		}
+		return a;
+	}
+
+	public String nextLine() {
+		int c = read();
+		while (isSpaceChar(c))
+			c = read();
+		StringBuilder res = new StringBuilder();
+		do {
+			res.appendCodePoint(c);
+			c = read();
+		} while (!isEndOfLine(c));
+		return res.toString();
+	}
+
+	public boolean isSpaceChar(int c) {
+		return c == ' ' || c == '\n' || c == '\r' || c == '\t' || c == -1;
+	}
+
+	private boolean isEndOfLine(int c) {
+		return c == '\n' || c == '\r' || c == -1;
 	}
 }
