e63cf48cc638a7704d347787abe8ce6891ae1416
qazyj <qazyj@naver.com>
2021-10-23 12:04:56 +0900

    유클리드 호제법을 통해 MN의 시간복잡도를 줄여줌

---
+++ 백준/수학/수학 6064 (카잉 달력) - SILVER 1.java
@@ -0,0 +1,140 @@
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.*;
+
+public class Main {
+	static StringBuilder sb;
+
+	public static void main(String[] args) throws Exception {
+		// TODO Auto-generated method stub
+		SetData();
+		System.out.println(sb);
+	}
+
+	private static void SetData() throws Exception {
+		InputReader in = new InputReader(System.in);
+
+		int testcase = in.nextInt();
+		sb = new StringBuilder();
+
+		for (int i = 0; i < testcase; i++) {
+			int M = in.nextInt();
+			int N = in.nextInt();
+			int x = in.nextInt();
+			int y = in.nextInt();
+			int lcm = M * N / gcd(M, N);
+
+			sb.append(FindValue(lcm, M, N, x, y)).append("\n");
+		}
+	}
+
+	private static int gcd(int x, int y) {
+		if (x % y == 0)
+			return y;
+
+		return gcd(y, x % y);
+	}
+
+	private static int FindValue(int lcm, int M, int N, int x, int y) {
+		int answer = -1;
+		for (int i = 0; i * M < lcm; i++) {
+			if ((i * M + x - y) % N == 0) {
+				answer = i * M + x;
+				break;
+			}
+		}
+		return answer;
+	}
+}
+
+class InputReader {
+	private final InputStream stream;
+	private final byte[] buf = new byte[8192];
+	private int curChar, snumChars;
+
+	public InputReader(InputStream st) {
+		this.stream = st;
+	}
+
+	public int read() {
+		if (snumChars == -1)
+			throw new InputMismatchException();
+		if (curChar >= snumChars) {
+			curChar = 0;
+			try {
+				snumChars = stream.read(buf);
+			} catch (IOException e) {
+				throw new InputMismatchException();
+			}
+			if (snumChars <= 0)
+				return -1;
+		}
+		return buf[curChar++];
+	}
+
+	public int nextInt() {
+		int c = read();
+		while (isSpaceChar(c)) {
+			c = read();
+		}
+		int sgn = 1;
+		if (c == '-') {
+			sgn = -1;
+			c = read();
+		}
+		int res = 0;
+		do {
+			res *= 10;
+			res += c - '0';
+			c = read();
+		} while (!isSpaceChar(c));
+		return res * sgn;
+	}
+
+	public long nextLong() {
+		int c = read();
+		while (isSpaceChar(c)) {
+			c = read();
+		}
+		int sgn = 1;
+		if (c == '-') {
+			sgn = -1;
+			c = read();
+		}
+		long res = 0;
+		do {
+			res *= 10;
+			res += c - '0';
+			c = read();
+		} while (!isSpaceChar(c));
+		return res * sgn;
+	}
+
+	public int[] nextIntArray(int n) {
+		int a[] = new int[n];
+		for (int i = 0; i < n; i++) {
+			a[i] = nextInt();
+		}
+		return a;
+	}
+
+	public String nextLine() {
+		int c = read();
+		while (isSpaceChar(c))
+			c = read();
+		StringBuilder res = new StringBuilder();
+		do {
+			res.appendCodePoint(c);
+			c = read();
+		} while (!isEndOfLine(c));
+		return res.toString();
+	}
+
+	public boolean isSpaceChar(int c) {
+		return c == ' ' || c == '\n' || c == '\r' || c == '\t' || c == -1;
+	}
+
+	private boolean isEndOfLine(int c) {
+		return c == '\n' || c == '\r' || c == -1;
+	}
+}
