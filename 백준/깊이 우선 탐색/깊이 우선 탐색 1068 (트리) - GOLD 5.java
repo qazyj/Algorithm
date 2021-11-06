09771941f8e9de0d055a9a08f3ac0f0f4a4313f6
qazyj <qazyj@naver.com>
2021-11-06 15:58:43 +0900

    dfs

---
+++ 백준/깊이 우선 탐색/깊이 우선 탐색 1068 (트리) - GOLD 5.java
@@ -0,0 +1,160 @@
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.*;
+
+public class Main {
+	static int n, delete;
+	static int[] parent;
+	static int answer;
+	static boolean[] check;
+
+	public static void main(String[] args) throws Exception {
+		SetData();
+		System.out.println(answer);
+	}
+
+	// 데이터
+	private static void SetData() throws Exception {
+		InputReader in = new InputReader(System.in);
+
+		n = in.nextInt();
+		parent = new int[n];
+		int root = 0;
+		for(int i = 0; i < n; i++) {
+			parent[i] = in.nextInt();
+			if(parent[i] == -1) root = i;
+		}
+		delete = in.nextInt();
+
+		deleteNode(delete);
+
+		answer = 0;
+		check = new boolean[n];
+		countLeaf(root);
+	}
+
+	public static void deleteNode(int d) {
+		parent[d] = -2;
+		for(int i = 0; i < n; i++) {
+			if(parent[i] == d) {
+				deleteNode(i);
+			}
+		}
+	}
+
+	public static void countLeaf(int s) {
+		boolean isLeaf = true;
+		check[s] = true;
+		if(parent[s] != -2) {
+			for(int i = 0; i < n; i++) {
+				if(parent[i] == s && check[i] == false) {
+					countLeaf(i);
+					isLeaf = false;
+				}
+			}
+			if(isLeaf) answer++;
+		}
+	}
+}
+
+class Node {
+	int x;
+	int y;
+
+	public Node(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+}
+
+class InputReader {
+	private final InputStream stream;
+	private final byte[] buf = new byte[8192];
+	private int curChar, snumChars;
+
+	public InputReader(InputStream st) {
+		this.stream = st;
+	}
+
+	public int read() {
+		if (snumChars == -1)
+			throw new InputMismatchException();
+		if (curChar >= snumChars) {
+			curChar = 0;
+			try {
+				snumChars = stream.read(buf);
+			} catch (IOException e) {
+				throw new InputMismatchException();
+			}
+			if (snumChars <= 0)
+				return -1;
+		}
+		return buf[curChar++];
+	}
+
+	public int nextInt() {
+		int c = read();
+		while (isSpaceChar(c)) {
+			c = read();
+		}
+		int sgn = 1;
+		if (c == '-') {
+			sgn = -1;
+			c = read();
+		}
+		int res = 0;
+		do {
+			res *= 10;
+			res += c - '0';
+			c = read();
+		} while (!isSpaceChar(c));
+		return res * sgn;
+	}
+
+	public long nextLong() {
+		int c = read();
+		while (isSpaceChar(c)) {
+			c = read();
+		}
+		int sgn = 1;
+		if (c == '-') {
+			sgn = -1;
+			c = read();
+		}
+		long res = 0;
+		do {
+			res *= 10;
+			res += c - '0';
+			c = read();
+		} while (!isSpaceChar(c));
+		return res * sgn;
+	}
+
+	public int[] nextIntArray(int n) {
+		int a[] = new int[n];
+		for (int i = 0; i < n; i++) {
+			a[i] = nextInt();
+		}
+		return a;
+	}
+
+	public String nextLine() {
+		int c = read();
+		while (isSpaceChar(c))
+			c = read();
+		StringBuilder res = new StringBuilder();
+		do {
+			res.appendCodePoint(c);
+			c = read();
+		} while (!isEndOfLine(c));
+		return res.toString();
+	}
+
+	public boolean isSpaceChar(int c) {
+		return c == ' ' || c == '\n' || c == '\r' || c == '\t' || c == -1;
+	}
+
+	private boolean isEndOfLine(int c) {
+		return c == '\n' || c == '\r' || c == -1;
+	}
+}
