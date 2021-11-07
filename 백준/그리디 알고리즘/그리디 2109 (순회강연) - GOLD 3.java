958ce7b8a3df4cb84eb3cf18c147c2d1ea2c69b9
qazyj <qazyj@naver.com>
2021-11-07 14:19:56 +0900

    그리디

---
+++ 백준/그리디 알고리즘/그리디 2109 (순회강연) - GOLD 3.java
@@ -0,0 +1,147 @@
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.*;
+
+public class Main {
+	static int n, answer;
+
+	public static void main(String[] args) throws Exception {
+		SetData();
+		System.out.println(answer);
+	}
+
+	// 데이터
+	private static void SetData() throws Exception {
+		InputReader in = new InputReader(System.in);
+
+		n = in.nextInt();
+		answer = 0;
+		PriorityQueue<Node> pq = new PriorityQueue<>();
+
+		for(int i = 0; i < n; i++) {
+			pq.add(new Node(in.nextInt(), in.nextInt()));
+		}
+
+		boolean[] check = new boolean[10001];
+		while(!pq.isEmpty()){
+			Node node = pq.poll();
+			for(int i = node.day; i >= 1; i--){
+				if(!check[i]){
+					answer += node.pay;
+					check[i] = true;
+					break;
+				}
+			}
+		}
+	}
+}
+
+class Node implements Comparable <Node>{
+	int day;
+	int pay;
+
+	public Node(int pay, int day) {
+		this.day = day;
+		this.pay = pay;
+	}
+
+	@Override
+	public int compareTo(Node o) {
+		if(this.pay == o.pay){
+			return o.day - this.day;
+		}
+		return o.pay - this.pay;
+	}
+}
+
+class InputReader {
+	private final InputStream stream;
+	private final byte[] buf = new byte[8192];
+	private int curChar, snumChars;
+
+	public InputReader(InputStream st) {
+		this.stream = st;
+	}
+
+	public int read() {
+		if (snumChars == -1)
+			throw new InputMismatchException();
+		if (curChar >= snumChars) {
+			curChar = 0;
+			try {
+				snumChars = stream.read(buf);
+			} catch (IOException e) {
+				throw new InputMismatchException();
+			}
+			if (snumChars <= 0)
+				return -1;
+		}
+		return buf[curChar++];
+	}
+
+	public int nextInt() {
+		int c = read();
+		while (isSpaceChar(c)) {
+			c = read();
+		}
+		int sgn = 1;
+		if (c == '-') {
+			sgn = -1;
+			c = read();
+		}
+		int res = 0;
+		do {
+			res *= 10;
+			res += c - '0';
+			c = read();
+		} while (!isSpaceChar(c));
+		return res * sgn;
+	}
+
+	public long nextLong() {
+		int c = read();
+		while (isSpaceChar(c)) {
+			c = read();
+		}
+		int sgn = 1;
+		if (c == '-') {
+			sgn = -1;
+			c = read();
+		}
+		long res = 0;
+		do {
+			res *= 10;
+			res += c - '0';
+			c = read();
+		} while (!isSpaceChar(c));
+		return res * sgn;
+	}
+
+	public int[] nextIntArray(int n) {
+		int a[] = new int[n];
+		for (int i = 0; i < n; i++) {
+			a[i] = nextInt();
+		}
+		return a;
+	}
+
+	public String nextLine() {
+		int c = read();
+		while (isSpaceChar(c))
+			c = read();
+		StringBuilder res = new StringBuilder();
+		do {
+			res.appendCodePoint(c);
+			c = read();
+		} while (!isEndOfLine(c));
+		return res.toString();
+	}
+
+	public boolean isSpaceChar(int c) {
+		return c == ' ' || c == '\n' || c == '\r' || c == '\t' || c == -1;
+	}
+
+	private boolean isEndOfLine(int c) {
+		return c == '\n' || c == '\r' || c == -1;
+	}
+}
