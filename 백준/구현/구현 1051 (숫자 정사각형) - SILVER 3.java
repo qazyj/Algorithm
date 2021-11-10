be6f7bd20071cc9c0073aa8c610129185d13804c
qazyj <qazyj@naver.com>
2021-11-10 13:48:34 +0900

    모든 인덱스 별로 가장 크게 나올 수 있는 정사각형 크기 구해줌

---
+++ 백준/구현/구현 1051 (숫자 정사각형) - SILVER 3.java
@@ -0,0 +1,143 @@
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.*;
+
+public class Main {
+	static int N, M, answer;
+	static int[][] array;
+
+	public static void main(String[] args) throws Exception {
+		SetData();
+		System.out.println(answer);
+	}
+
+	// 데이터
+	private static void SetData() throws Exception {
+		InputReader in = new InputReader(System.in);
+
+		N = in.nextInt();
+		M = in.nextInt();
+		answer = 1;
+		array = new int[N][M];
+
+		for(int i = 0; i < N; i++) {
+			String s = in.nextLine();
+			for(int j = 0 ; j < M; j++) {
+				array[i][j] = s.charAt(j) - '0';
+			}
+		}
+
+		for(int i = 0; i < N; i++) {
+			for(int j = 0; j < M; j++) {
+				answer = Math.max(answer, rectangle(i,j));
+			}
+		}
+	}
+
+	private static int rectangle(int a, int b) {
+		int max = 1;
+		int min = N-a;
+		min = Math.min(min, M-b);
+		for(int i = 1; i < min; i++) {
+			if(array[a][b] == array[a+i][b]
+					&& array[a][b] == array[a][b+i]
+					&& array[a][b] == array[a+i][b+i]) {
+				max = i+1;
+			}
+		}
+
+		return max*max;
+	}
+}
+
+class InputReader {
+	private final InputStream stream;
+	private final byte[] buf = new byte[8192];
+	private int curChar, snumChars;
+
+	public InputReader(InputStream st) {
+		this.stream = st;
+	}
+
+	public int read() {
+		if (snumChars == -1)
+			throw new InputMismatchException();
+		if (curChar >= snumChars) {
+			curChar = 0;
+			try {
+				snumChars = stream.read(buf);
+			} catch (IOException e) {
+				throw new InputMismatchException();
+			}
+			if (snumChars <= 0)
+				return -1;
+		}
+		return buf[curChar++];
+	}
+
+	public int nextInt() {
+		int c = read();
+		while (isSpaceChar(c)) {
+			c = read();
+		}
+		int sgn = 1;
+		if (c == '-') {
+			sgn = -1;
+			c = read();
+		}
+		int res = 0;
+		do {
+			res *= 10;
+			res += c - '0';
+			c = read();
+		} while (!isSpaceChar(c));
+		return res * sgn;
+	}
+
+	public long nextLong() {
+		int c = read();
+		while (isSpaceChar(c)) {
+			c = read();
+		}
+		int sgn = 1;
+		if (c == '-') {
+			sgn = -1;
+			c = read();
+		}
+		long res = 0;
+		do {
+			res *= 10;
+			res += c - '0';
+			c = read();
+		} while (!isSpaceChar(c));
+		return res * sgn;
+	}
+
+	public int[] nextIntArray(int n) {
+		int a[] = new int[n];
+		for (int i = 0; i < n; i++) {
+			a[i] = nextInt();
+		}
+		return a;
+	}
+
+	public String nextLine() {
+		int c = read();
+		while (isSpaceChar(c))
+			c = read();
+		StringBuilder res = new StringBuilder();
+		do {
+			res.appendCodePoint(c);
+			c = read();
+		} while (!isEndOfLine(c));
+		return res.toString();
+	}
+
+	public boolean isSpaceChar(int c) {
+		return c == ' ' || c == '\n' || c == '\r' || c == '\t' || c == -1;
+	}
+
+	private boolean isEndOfLine(int c) {
+		return c == '\n' || c == '\r' || c == -1;
+	}
+}
