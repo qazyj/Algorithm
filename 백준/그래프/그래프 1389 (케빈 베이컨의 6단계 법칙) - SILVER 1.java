185a657630fed239cc40160e2b0cbeacfa9b2d9d
qazyj <qazyj@naver.com>
2022-02-17 23:47:33 +0900

    플로이드 와샬

---
+++ 백준/그래프/그래프 1389 (케빈 베이컨의 6단계 법칙) - SILVER 1.java
@@ -0,0 +1,152 @@
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.*;
+
+public class Main {
+	static int N, M, answer;
+	static int[][] array;
+
+	public static void main(String[] args) throws Exception {
+		SetData();
+		System.out.println(answer);
+	}
+
+	private static void SetData() throws Exception {
+		InputReader in = new InputReader(System.in);
+
+		N = in.nextInt();
+		M = in.nextInt();
+		array = new int[N+1][N+1];
+		answer = 0;
+
+		for(int i = 1; i <= N; i++) {
+			for(int j = 1; j <= N; j++) {
+				array[i][j] = 500000;
+
+				if(i == j) array[i][j] = 0;
+			}
+		}
+
+		for (int i = 0; i < M; i++) {
+			int x = in.nextInt();
+			int y = in.nextInt();
+
+			array[x][y] = array[y][x] = 1;
+		}
+
+		for (int k = 1; k <= N; k++) {
+			for (int i = 1; i <= N; i++) {
+				for (int j = 1; j <= N; j++) {
+					if (array[i][j] > array[i][k] + array[k][j]) {
+						array[i][j] = array[i][k] + array[k][j];
+					}
+				}
+			}
+		}
+
+		int result = 500000;
+		for (int i = 1; i <= N; i++) {
+			int total = 0;
+			for (int j = 1; j <= N; j++) {
+				total += array[i][j];
+			}
+
+			if (result > total) {
+				result = total;
+				answer = i;
+			}
+		}
+	}
+}
+
+class InputReader {
+	private final InputStream stream;
+	private final byte[] buf = new byte[8192];
+	private int curChar, snumChars;
+
+	public InputReader(InputStream st) {
+		this.stream = st;
+	}
+
+	public int read() {
+		if (snumChars == -1)
+			throw new InputMismatchException();
+		if (curChar >= snumChars) {
+			curChar = 0;
+			try {
+				snumChars = stream.read(buf);
+			} catch (IOException e) {
+				throw new InputMismatchException();
+			}
+			if (snumChars <= 0)
+				return -1;
+		}
+		return buf[curChar++];
+	}
+
+	public int nextInt() {
+		int c = read();
+		while (isSpaceChar(c)) {
+			c = read();
+		}
+		int sgn = 1;
+		if (c == '-') {
+			sgn = -1;
+			c = read();
+		}
+		int res = 0;
+		do {
+			res *= 10;
+			res += c - '0';
+			c = read();
+		} while (!isSpaceChar(c));
+		return res * sgn;
+	}
+
+	public long nextLong() {
+		int c = read();
+		while (isSpaceChar(c)) {
+			c = read();
+		}
+		int sgn = 1;
+		if (c == '-') {
+			sgn = -1;
+			c = read();
+		}
+		long res = 0;
+		do {
+			res *= 10;
+			res += c - '0';
+			c = read();
+		} while (!isSpaceChar(c));
+		return res * sgn;
+	}
+
+	public int[] nextIntArray(int n) {
+		int a[] = new int[n];
+		for (int i = 0; i < n; i++) {
+			a[i] = nextInt();
+		}
+		return a;
+	}
+
+	public String nextLine() {
+		int c = read();
+		while (isSpaceChar(c))
+			c = read();
+		StringBuilder res = new StringBuilder();
+		do {
+			res.appendCodePoint(c);
+			c = read();
+		} while (!isEndOfLine(c));
+		return res.toString();
+	}
+
+	public boolean isSpaceChar(int c) {
+		return c == ' ' || c == '\n' || c == '\r' || c == '\t' || c == -1;
+	}
+
+	private boolean isEndOfLine(int c) {
+		return c == '\n' || c == '\r' || c == -1;
+	}
+}
