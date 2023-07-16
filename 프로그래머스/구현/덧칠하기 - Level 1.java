ca6bfade11d28b22c2eda444a0ef26791f04ee15
김영진 <qazyj@naver.com>
2023-07-16 15:30:00 +0900

    Add 🌟⭐⭐⭐⭐

---
+++ 프로그래머스/구현/덧칠하기 - Level 1.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int solution(int n, int m, int[] sections) {
+        int start = sections[0];
+        int end = sections[0] + (m-1);
+        int answer = 1;
+
+        for(int section : sections){
+            if(section >= start && section <= end)
+                continue;
+            else {
+                start = section;
+                end = section + (m-1);
+                answer++;
+            }
+
+        }
+        return answer;
+    }
+}
