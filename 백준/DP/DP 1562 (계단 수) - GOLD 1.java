441f3d5ce8aed38dd3aa1f397c457930bf8ae28a
김영진 <qazyj@naver.com>
2024-01-16 17:52:07 +0900

    Add 🌟🌟🌟🌟🌟

---
+++ 백준/DP/DP 1562 (계단 수) - GOLD 1.java
@@ -0,0 +1,142 @@
+import java.io.*;
+import java.math.BigInteger;
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.IntStream;
+
+public class Main {
+	static int MOD = 1_000_000_000;
+
+	public static void main(String[] args) throws Exception {
+		InputReader in = new InputReader(System.in);
+
+		int n = in.nextInt();
+		long[][][] dp = new long[n+1][11][1<<10];
+
+		// 1<<i : 0~9 비트마스킹
+		for(int i=1; i<10; i++) {
+			dp[1][i][1<<i] =1;
+		}
+
+		/**
+		 *  i : i자리 숫자
+		 *  j 끝나는 숫자
+		 *  k 마킹된 숫자
+		 */
+		for(int i=2; i<n+1; i++) {
+			for(int j=0; j<10; j++) {
+				for(int k=0; k<1024; k++) {
+					int bit = k | (1 << j);
+					if(j==0) {
+						dp[i][j][bit] = (dp[i][j][bit] + dp[i-1][j+1][k]) % MOD;
+					}
+					else if(j==9) {
+						dp[i][j][bit] = (dp[i][j][bit] + dp[i-1][j-1][k]) % MOD;
+					}
+					else {
+						dp[i][j][bit] = (dp[i][j][bit] + dp[i-1][j+1][k]+ dp[i-1][j-1][k]) % MOD;
+					}
+				}
+			}
+		}
+
+		long sum = 0;
+		for(int i=0; i<10; i++) {
+			sum = (sum + dp[n][i][1023]) % MOD;
+		}
+		System.out.println(sum);
+	}
+}
+
+class InputReader {
+	private final InputStream stream;
+	private final byte[] buf = new byte[8192];
+	private int curChar, snumChars;
+
+	public InputReader(InputStream st) {
+		this.stream = st;
+	}
+
+	public int read() {
+		if (snumChars == -1)
+			throw new InputMismatchException();
+		if (curChar >= snumChars) {
+			curChar = 0;
+			try {
+				snumChars = stream.read(buf);
+			} catch (IOException e) {
+				throw new InputMismatchException();
+			}
+			if (snumChars <= 0)
+				return -1;
+		}
+		return buf[curChar++];
+	}
+
+	public int nextInt() {
+		int c = read();
+		while (isSpaceChar(c)) {
+			c = read();
+		}
+		int sgn = 1;
+		if (c == '-') {
+			sgn = -1;
+			c = read();
+		}
+		int res = 0;
+		do {
+			res *= 10;
+			res += c - '0';
+			c = read();
+		} while (!isSpaceChar(c));
+		return res * sgn;
+	}
+
+	public long nextLong() {
+		int c = read();
+		while (isSpaceChar(c)) {
+			c = read();
+		}
+		int sgn = 1;
+		if (c == '-') {
+			sgn = -1;
+			c = read();
+		}
+		long res = 0;
+		do {
+			res *= 10;
+			res += c - '0';
+			c = read();
+		} while (!isSpaceChar(c));
+		return res * sgn;
+	}
+
+	public int[] nextIntArray(int n) {
+		int a[] = new int[n];
+		for (int i = 0; i < n; i++) {
+			a[i] = nextInt();
+		}
+		return a;
+	}
+
+	public String nextLine() {
+		int c = read();
+		while (isSpaceChar(c))
+			c = read();
+		StringBuilder res = new StringBuilder();
+		do {
+			res.appendCodePoint(c);
+			c = read();
+		} while (!isEndOfLine(c));
+		return res.toString();
+	}
+
+	public boolean isSpaceChar(int c) {
+		return c == ' ' || c == '\n' || c == '\r' || c == '\t' || c == -1;
+	}
+
+	private boolean isEndOfLine(int c) {
+		return c == '\n' || c == '\r' || c == -1;
+	}
+}
