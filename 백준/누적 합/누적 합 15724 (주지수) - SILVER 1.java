164504e3daa967e9844c01cb991b3d4fc234b281
김영진 <qazyj@naver.com>
2024-02-07 23:28:45 +0900

    Add 🌟⭐️⭐️⭐️⭐️

---
+++ 백준/누적 합/누적 합 15724 (주지수) - SILVER 1.java
@@ -0,0 +1,131 @@
+import java.io.*;
+import java.math.BigInteger;
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        InputReader in = new InputReader(System.in);
+
+        int N = in.nextInt();
+        int M = in.nextInt();
+
+        int[][] map = new int[N+1][M+1];
+
+        for(int i = 1; i <= N; i++)
+        {
+            for(int j = 1; j <= M; j++)
+            {
+                map[i][j] = in.nextInt() + map[i-1][j] + map[i][j-1] - map[i-1][j-1];
+            }
+        }
+
+        int K = in.nextInt();
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < K; ++i)
+        {
+            int x1 = in.nextInt();
+            int y1 = in.nextInt();
+            int x2 = in.nextInt();
+            int y2 = in.nextInt();
+
+            int sum = map[x2][y2] - map[x2][y1-1] - map[x1-1][y2] + map[x1-1][y1-1];
+            sb.append(sum+"\n");
+        }
+        System.out.println(sb);
+    }
+}
+
+class InputReader {
+    private final InputStream stream;
+    private final byte[] buf = new byte[8192];
+    private int curChar, snumChars;
+
+    public InputReader(InputStream st) {
+        this.stream = st;
+    }
+
+    public int read() {
+        if (snumChars == -1)
+            throw new InputMismatchException();
+        if (curChar >= snumChars) {
+            curChar = 0;
+            try {
+                snumChars = stream.read(buf);
+            } catch (IOException e) {
+                throw new InputMismatchException();
+            }
+            if (snumChars <= 0)
+                return -1;
+        }
+        return buf[curChar++];
+    }
+
+    public int nextInt() {
+        int c = read();
+        while (isSpaceChar(c)) {
+            c = read();
+        }
+        int sgn = 1;
+        if (c == '-') {
+            sgn = -1;
+            c = read();
+        }
+        int res = 0;
+        do {
+            res *= 10;
+            res += c - '0';
+            c = read();
+        } while (!isSpaceChar(c));
+        return res * sgn;
+    }
+
+    public long nextLong() {
+        int c = read();
+        while (isSpaceChar(c)) {
+            c = read();
+        }
+        int sgn = 1;
+        if (c == '-') {
+            sgn = -1;
+            c = read();
+        }
+        long res = 0;
+        do {
+            res *= 10;
+            res += c - '0';
+            c = read();
+        } while (!isSpaceChar(c));
+        return res * sgn;
+    }
+
+    public int[] nextIntArray(int n) {
+        int a[] = new int[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = nextInt();
+        }
+        return a;
+    }
+
+    public String nextLine() {
+        int c = read();
+        while (isSpaceChar(c))
+            c = read();
+        StringBuilder res = new StringBuilder();
+        do {
+            res.appendCodePoint(c);
+            c = read();
+        } while (!isEndOfLine(c));
+        return res.toString();
+    }
+
+    public boolean isSpaceChar(int c) {
+        return c == ' ' || c == '\n' || c == '\r' || c == '\t' || c == -1;
+    }
+
+    private boolean isEndOfLine(int c) {
+        return c == '\n' || c == '\r' || c == -1;
+    }
+}
