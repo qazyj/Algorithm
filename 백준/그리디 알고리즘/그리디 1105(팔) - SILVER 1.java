878499edee65f823d024822570964c89be26a34d
김영진 <qazyj@naver.com>
2024-03-01 16:50:38 +0900

    Add 🌟🌟⭐️⭐️⭐️

---
+++ 백준/그리디 알고리즘/그리디 1105(팔) - SILVER 1.java
@@ -0,0 +1,129 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		InputReader in = new InputReader(System.in);
+
+		String[] s = in.nextLine().split(" ");
+		char[] l = s[0].toCharArray();
+		char[] r = s[1].toCharArray();
+
+		if(l.length != r.length) {
+			System.out.println(0);
+			System.exit(0);
+		}
+
+		int count = 0;
+		boolean check = false;
+		for(int i = 0; i < l.length; i++) {
+			if(l[i] == r[i]) {
+				if(!check && l[i] == '8') count++;
+			} else {
+				check = true;
+			}
+		}
+		System.out.println(count);
+	}
+
+	public static int getCount(int num) {
+		int count = 0;
+		while(num != 0) {
+			if(num % 10 == 8) count++;
+			num /= 10;
+		}
+		return count;
+	}
+}
+
+class InputReader {
+	private final InputStream stream;
+	private final byte[] buf = new byte[8192];
+	private int curChar, snumChars;
+
+	public InputReader(InputStream st) {
+		this.stream = st;
+	}
+
+	public int read() {
+		if (snumChars == -1)
+			throw new InputMismatchException();
+		if (curChar >= snumChars) {
+			curChar = 0;
+			try {
+				snumChars = stream.read(buf);
+			} catch (IOException e) {
+				throw new InputMismatchException();
+			}
+			if (snumChars <= 0)
+				return -1;
+		}
+		return buf[curChar++];
+	}
+
+	public int nextInt() {
+		int c = read();
+		while (isSpaceChar(c)) {
+			c = read();
+		}
+		int sgn = 1;
+		if (c == '-') {
+			sgn = -1;
+			c = read();
+		}
+		int res = 0;
+		do {
+			res *= 10;
+			res += c - '0';
+			c = read();
+		} while (!isSpaceChar(c));
+		return res * sgn;
+	}
+
+	public long nextLong() {
+		int c = read();
+		while (isSpaceChar(c)) {
+			c = read();
+		}
+		int sgn = 1;
+		if (c == '-') {
+			sgn = -1;
+			c = read();
+		}
+		long res = 0;
+		do {
+			res *= 10;
+			res += c - '0';
+			c = read();
+		} while (!isSpaceChar(c));
+		return res * sgn;
+	}
+
+	public int[] nextIntArray(int n) {
+		int a[] = new int[n];
+		for (int i = 0; i < n; i++) {
+			a[i] = nextInt();
+		}
+		return a;
+	}
+
+	public String nextLine() {
+		int c = read();
+		while (isSpaceChar(c))
+			c = read();
+		StringBuilder res = new StringBuilder();
+		do {
+			res.appendCodePoint(c);
+			c = read();
+		} while (!isEndOfLine(c));
+		return res.toString();
+	}
+
+	public boolean isSpaceChar(int c) {
+		return c == ' ' || c == '\n' || c == '\r' || c == '\t' || c == -1;
+	}
+
+	private boolean isEndOfLine(int c) {
+		return c == '\n' || c == '\r' || c == -1;
+	}
+}
