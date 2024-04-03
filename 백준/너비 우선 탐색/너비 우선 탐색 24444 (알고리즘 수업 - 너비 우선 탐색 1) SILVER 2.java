4bcd64190f01ea4810e80e0b451d8962c950d3ed
김영진 <qazyj@naver.com>
2024-04-03 11:24:06 +0900

    Add 🌟⭐️⭐️⭐️⭐️

---
+++ 백준/너비 우선 탐색/너비 우선 탐색 24444 (알고리즘 수업 - 너비 우선 탐색 1) SILVER 2.java
@@ -0,0 +1,147 @@
+import java.io.*;
+import java.sql.Array;
+import java.util.*;
+
+public class Main {
+	static List<Integer>[] list;
+	static int[] answer;
+
+	public static void main(String[] args) throws Exception {
+		InputReader in = new InputReader(System.in);
+
+		int n = in.nextInt();
+		int m = in.nextInt();
+		int r = in.nextInt();
+		answer = new int[n+1];
+		list = new ArrayList[n+1];
+		for(int i = 1; i <= n; i++) list[i] = new ArrayList<>();
+
+		for(int i = 0; i < m; i++) {
+			int a = in.nextInt();
+			int b = in.nextInt();
+
+			list[a].add(b);
+			list[b].add(a);
+		}
+		for(int i = 1; i <= n; i++) Collections.sort(list[i], Collections.reverseOrder());
+
+		bfs(n, r);
+		StringBuilder sb = new StringBuilder();
+		for(int i = 1; i <= n; i++) {
+			sb.append(answer[i]).append("\n");
+		}
+		System.out.println(sb);
+	}
+
+	public static void bfs(int n, int start){
+		Queue<Integer> q = new LinkedList<>();
+		boolean[] visited = new boolean[n+1];
+		visited[start] = true;
+		q.add(start);
+		int order = 1;
+		answer[start] = order++;
+		while(!q.isEmpty()) {
+			int cur = q.poll();
+
+			for(int next : list[cur]) {
+				if(visited[next]) continue;
+
+				answer[next] = order++;
+				visited[next] = true;
+				q.add(next);
+			}
+		}
+	}
+}
+
+class InputReader {
+	private final InputStream stream;
+	private final byte[] buf = new byte[8192];
+	private int curChar, snumChars;
+
+	public InputReader(InputStream st) {
+		this.stream = st;
+	}
+
+	public int read() {
+		if (snumChars == -1)
+			throw new InputMismatchException();
+		if (curChar >= snumChars) {
+			curChar = 0;
+			try {
+				snumChars = stream.read(buf);
+			} catch (IOException e) {
+				throw new InputMismatchException();
+			}
+			if (snumChars <= 0)
+				return -1;
+		}
+		return buf[curChar++];
+	}
+
+	public int nextInt() {
+		int c = read();
+		while (isSpaceChar(c)) {
+			c = read();
+		}
+		int sgn = 1;
+		if (c == '-') {
+			sgn = -1;
+			c = read();
+		}
+		int res = 0;
+		do {
+			res *= 10;
+			res += c - '0';
+			c = read();
+		} while (!isSpaceChar(c));
+		return res * sgn;
+	}
+
+	public long nextLong() {
+		int c = read();
+		while (isSpaceChar(c)) {
+			c = read();
+		}
+		int sgn = 1;
+		if (c == '-') {
+			sgn = -1;
+			c = read();
+		}
+		long res = 0;
+		do {
+			res *= 10;
+			res += c - '0';
+			c = read();
+		} while (!isSpaceChar(c));
+		return res * sgn;
+	}
+
+	public int[] nextIntArray(int n) {
+		int a[] = new int[n];
+		for (int i = 0; i < n; i++) {
+			a[i] = nextInt();
+		}
+		return a;
+	}
+
+	public String nextLine() {
+		int c = read();
+		while (isSpaceChar(c))
+			c = read();
+		StringBuilder res = new StringBuilder();
+		do {
+			res.appendCodePoint(c);
+			c = read();
+		} while (!isEndOfLine(c));
+		return res.toString();
+	}
+
+	public boolean isSpaceChar(int c) {
+		return c == ' ' || c == '\n' || c == '\r' || c == '\t' || c == -1;
+	}
+
+	private boolean isEndOfLine(int c) {
+		return c == '\n' || c == '\r' || c == -1;
+	}
+}
