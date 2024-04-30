bbe3971542691595f494f1d53b4c68e7add6bb57
김영진 <qazyj@naver.com>
2024-05-01 00:01:32 +0900

    Add 🌟🌟⭐️⭐️⭐️

--- 프로그래머스/자료구조/뒤에 있는 큰수 찾기 - Level 2.java
+++ 프로그래머스/자료구조/뒤에 있는 큰수 찾기 - Level 2.java
@@ -1,6 +1,7 @@
 import java.util.*;
 
-public int[] solution(int[] numbers) {
+class Solution {
+    public int[] solution(int[] numbers) {
         Stack<Integer> stack = new Stack<>();
 
         int[] answer = new int[numbers.length];
@@ -8,14 +9,15 @@ public int[] solution(int[] numbers) {
         stack.push(0);
 
         for (int i = 1; i < numbers.length; i++) {
-        while (!stack.isEmpty() && numbers[stack.peek()] < numbers[i]) {
-        answer[stack.pop()] = numbers[i];
-        }
-        stack.push(i);
+            while (!stack.isEmpty() && numbers[stack.peek()] < numbers[i]) {
+                answer[stack.pop()] = numbers[i];
+            }
+            stack.push(i);
         }
         while (!stack.isEmpty()) {
-        answer[stack.pop()] = -1;
+            answer[stack.pop()] = -1;
         }
 
         return answer;
-        }
+    }
+}
