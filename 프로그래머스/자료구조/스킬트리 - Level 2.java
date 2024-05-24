729f2d0d01c0fe44d7f22eee8fc9da645d1bc5a5
김영진 <qazyj@naver.com>
2024-05-24 21:26:06 +0900

    Improve code

--- 프로그래머스/자료구조/스킬트리 - Level 2.java
+++ 프로그래머스/자료구조/스킬트리 - Level 2.java
@@ -1,38 +1,21 @@
-import java.util.*;
-
 class Solution {
     public int solution(String skill, String[] skill_trees) {
-        Set<Integer>[] set = new HashSet[26];
-        for(int i = 0; i < 26; i++) {
-            set[i] = new HashSet<>();
-        }
-
-        for(int i = 0; i < skill.length(); i++) {
-            int index = skill.charAt(i)-'A';
-            for(int j = 0; j < i; j++) {
-                set[index].add(skill.charAt(j)-'A');
-            }
-        }
-
         int answer = 0;
-        for(int i = 0; i < skill_trees.length; i++) {
-            boolean[] isUsed = new boolean[26];
-            boolean check = true;
-            for(int j = 0; j < skill_trees[i].length(); j++) {
-                int now = skill_trees[i].charAt(j)-'A';
-                isUsed[now] = true;
-
-                if(set[now].isEmpty()) continue;
-
-                for(int mustUsed : set[now]) {
-                    if(isUsed[mustUsed]) continue;
-
-                    check = false;
+        for (String skillTree : skill_trees) {
+            int learningIndex = 0;
+            boolean canUse = true;
+            for (char curSkill : skillTree.toCharArray()) {
+                int skillIndex = skill.indexOf(curSkill);
+                if (skillIndex == -1)
+                    continue;
+                else if (skillIndex == learningIndex)
+                    learningIndex++;
+                else {
+                    canUse = false;
                     break;
                 }
-                if(!check) break;
             }
-            if(check) answer++;
+            if (canUse)  answer++;
         }
         return answer;
     }
