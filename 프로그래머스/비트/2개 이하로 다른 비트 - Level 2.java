55665f2dacab52d6dd0acd85f7c5d33fac1cf885
김영진 <qazyj@naver.com>
2024-06-17 13:43:57 +0900

    Add 🌟🌟⭐️⭐️⭐️

---
+++ 프로그래머스/비트/2개 이하로 다른 비트 - Level 2.java
@@ -0,0 +1,10 @@
+class Solution {
+    public long[] solution(long[] numbers) {
+        long[] answer = new long[numbers.length];
+        for (int i = 0; i < numbers.length; i++) {
+            answer[i] = numbers[i]+1;
+            answer[i] += (answer[i] ^ numbers[i]) >> 2;
+        }
+        return answer;
+    }
+}
