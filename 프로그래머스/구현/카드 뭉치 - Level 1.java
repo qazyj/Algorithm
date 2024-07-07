7477caafdcbd4bc92daf04d9ea29621f6a3254b1
김영진 <qazyj@naver.com>
2024-07-07 14:02:46 +0900

    Add 🌟⭐️⭐️⭐️⭐️

---
+++ 프로그래머스/구현/카드 뭉치 - Level 1.java
@@ -0,0 +1,19 @@
+class Solution {
+    public String solution(String[] cards1, String[] cards2, String[] goal) {
+        boolean canFormWord = true;
+        int card1Index = 0;
+        int card2Index = 0;
+        for(int i = 0; i < goal.length; i++) {
+            if(card1Index < cards1.length && goal[i].equals(cards1[card1Index])) {
+                card1Index++;
+            } else if(card2Index < cards2.length && goal[i].equals(cards2[card2Index])) {
+                card2Index++;
+            } else {
+                canFormWord = false;
+                break;
+            }
+        }
+        if(canFormWord) return "Yes";
+        else return "No";
+    }
+}
