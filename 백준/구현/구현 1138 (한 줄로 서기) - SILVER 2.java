59d520532f635ebdc75d3e0b21f4d2457cb15ff1
김영진 <qazyj@naver.com>
2024-08-29 23:42:50 +0900

    Add 🌟🌟⭐⭐⭐

--- 백준/구현/구현 1138 (한 줄로 서기) - SILVER 2.java
+++ 백준/구현/구현 1138 (한 줄로 서기) - SILVER 2.java
@@ -1,127 +1,26 @@
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
+import java.util.StringTokenizer;
 import java.util.*;
 
-public class Main {
-	static int N, M, answer;
-	static int[][] array;
-	static StringBuilder sb;
+public class Solution {
 
-	public static void main(String[] args) throws Exception {
-		SetData();
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int N = Integer.parseInt(br.readLine());
+		int[] arr = new int[N+1];
+		List<Integer> list = new ArrayList<>();
+
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for(int i = 1; i<=N; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+
+		for(int i= N; i>=1; i--) {
+			array.add(arr[i], i);
+		}
+		StringBuilder sb = new StringBuilder();
+		for(int value : list) sb.append(value).append(" ");
 		System.out.println(sb);
-	}
-
-	// 데이터
-	private static void SetData() throws Exception {
-		InputReader in = new InputReader(System.in);
-
-		N = in.nextInt();
-		int [] tall = new int[N+1];
-		ArrayList<Integer> array = new ArrayList<>();
-		sb = new StringBuilder();
-
-		for(int i=1; i<= N; i++)
-			tall[i] = in.nextInt();
-
-		for(int i= N; i>=1; i--)
-			array.add(tall[i], i);
-
-
-		for(int temp : array) {
-			sb.append(temp+" ");
-		}
-	}
-}
-
-class InputReader {
-	private final InputStream stream;
-	private final byte[] buf = new byte[8192];
-	private int curChar, snumChars;
-
-	public InputReader(InputStream st) {
-		this.stream = st;
-	}
-
-	public int read() {
-		if (snumChars == -1)
-			throw new InputMismatchException();
-		if (curChar >= snumChars) {
-			curChar = 0;
-			try {
-				snumChars = stream.read(buf);
-			} catch (IOException e) {
-				throw new InputMismatchException();
-			}
-			if (snumChars <= 0)
-				return -1;
-		}
-		return buf[curChar++];
-	}
-
-	public int nextInt() {
-		int c = read();
-		while (isSpaceChar(c)) {
-			c = read();
-		}
-		int sgn = 1;
-		if (c == '-') {
-			sgn = -1;
-			c = read();
-		}
-		int res = 0;
-		do {
-			res *= 10;
-			res += c - '0';
-			c = read();
-		} while (!isSpaceChar(c));
-		return res * sgn;
-	}
-
-	public long nextLong() {
-		int c = read();
-		while (isSpaceChar(c)) {
-			c = read();
-		}
-		int sgn = 1;
-		if (c == '-') {
-			sgn = -1;
-			c = read();
-		}
-		long res = 0;
-		do {
-			res *= 10;
-			res += c - '0';
-			c = read();
-		} while (!isSpaceChar(c));
-		return res * sgn;
-	}
-
-	public int[] nextIntArray(int n) {
-		int a[] = new int[n];
-		for (int i = 0; i < n; i++) {
-			a[i] = nextInt();
-		}
-		return a;
-	}
-
-	public String nextLine() {
-		int c = read();
-		while (isSpaceChar(c))
-			c = read();
-		StringBuilder res = new StringBuilder();
-		do {
-			res.appendCodePoint(c);
-			c = read();
-		} while (!isEndOfLine(c));
-		return res.toString();
-	}
-
-	public boolean isSpaceChar(int c) {
-		return c == ' ' || c == '\n' || c == '\r' || c == '\t' || c == -1;
-	}
-
-	private boolean isEndOfLine(int c) {
-		return c == '\n' || c == '\r' || c == -1;
 	}
 }
