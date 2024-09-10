1c3d5eb3d8e6af6ef1100af866c35ca0640baca8
김영진 <qazyj@naver.com>
2024-09-10 23:33:00 +0900

    Add 🌟🌟⭐⭐⭐

--- 프로그래머스/2024 KAKAO BLIND RECRUITMENT/도넛과 막대 그래프 - Leve 2.java
+++ 프로그래머스/2024 KAKAO BLIND RECRUITMENT/도넛과 막대 그래프 - Leve 2.java
@@ -1,68 +1,79 @@
 import java.util.*;
 
 class Solution {
-	public int solution(String[] friends, String[] gifts) {
-		Map<String, Integer> map = new HashMap<>();
-		for(int i = 0; i < friends.length; i++) {
-			map.put(friends[i], i);
+	public int[] solution(int[][] edges) {
+		int n = 0;
+		for(int i = 0; i < edges.length; i++) {
+			n = Math.max(n, edges[i][0]);
+			n = Math.max(n, edges[i][1]);
 		}
 
-		int n = friends.length;
-		int[][] arr = new int[n][n];
-		for(String gift : gifts) {
-			String[] temp = gift.split(" ");
-			String a = temp[0];
-			String b = temp[1];
-			int aIndex = map.get(a);
-			int bIndex = map.get(b);
+		int[] count = new int[n+1];
 
-			arr[aIndex][bIndex]++;
+		List<Integer>[] list = new ArrayList[n+1];
+		for(int i = 0; i <= n; i++) {
+			list[i] = new ArrayList<>();
 		}
 
-		for(int i = 0 ;i < n; i++) {
-			for(int j = 0; j < n; j++) {
-				System.out.print(arr[i][j] + " ");
-			}
-			System.out.println();
+		for(int i = 0; i < edges.length; i++) {
+			list[edges[i][0]].add(edges[i][1]);
+			count[edges[i][0]]++;
+			count[edges[i][1]]--;
 		}
 
-		for(int i = 0; i < n; i++) {
+		int rootNode = 0;
+		for(int i = 1; i <= n; i++) {
+			if(count[i] <= 1) continue;
 
-			int give = 0;
-			for(int j = 0; j < n; j++) {
-				give += arr[i][j];
-			}
-
-			int take = 0;
-			for(int j = 0; j < n; j++) {
-				take += arr[j][i];
-			}
-
-			arr[i][i] = give - take;
+			rootNode = i;
+			break;
 		}
 
-		for(int i = 0; i < n; i++) {
-			System.out.print(arr[i][i] + " ");
-		}
-		System.out.println();
-
-		int answer = 0;
-		int[] gift = new int[n];
-		for(int i = 0; i < n; i++) {
-			for(int j = 0; j < n; j++) {
-				if(i == j) continue;
-
-				if(arr[i][j] > arr[j][i])  gift[i]++;
-				else if(arr[i][j] == arr[j][i] || (arr[i][j] == 0 && arr[j][i] == 0)) {
-					if(arr[i][i] > arr[j][j]) gift[i]++;
-				}
+		int[] answer = new int[]{rootNode,0,0,0};
+		for(int start : list[rootNode]) {
+			int[] temp = search(list, start);
+			if(temp[0] == temp[1]) {
+				// 도넛 : 정점 N, 간선 n
+				//System.out.println("도넛 : " + temp[0] +" " + temp[1]);
+				answer[1]++;
+			} else if(temp[0] == temp[1]+1) {
+				// 막대 : 정점 n, 간선 n-1
+				//System.out.println("막대 : " + temp[0] +" " + temp[1]);
+				answer[2]++;
+			} else if(temp[0] + 1 == temp[1]) {
+				// 8자 : 정점 2n+1, 간선 2n+2
+				//System.out.println("8자 : " + temp[0] +" " + temp[1]);
+				answer[3]++;
 			}
 		}
 
-		for(int i = 0; i < n; i++) {
-			answer = Math.max(answer, gift[i]);
-		}
 
 		return answer;
 	}
+
+	public int[] search(List<Integer>[] list, int start) {
+		Queue<Integer> q = new LinkedList<>();
+		q.add(start);
+		int[] temp = new int[2];
+		temp[0]++;
+		Set<Integer> set = new HashSet<>();
+		set.add(start);
+		int count = 0;
+		while(!q.isEmpty()) {
+			int cur = q.poll();
+			if(cur == start && count > 0) return temp;
+			if(list[cur].size() >= 2) return new int[]{0, 1};
+
+			for(int next : list[cur]) {
+				if(!set.contains(next)) temp[0]++;
+
+				count++;
+				set.add(next);
+				temp[1]++;
+				q.add(next);
+			}
+		}
+		return temp;
+	}
+
 }
