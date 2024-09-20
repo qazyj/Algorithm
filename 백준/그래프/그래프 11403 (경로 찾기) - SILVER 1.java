0c541d72a452bcfee568737df21b7dfb9fefaace
김영진 <qazyj@naver.com>
2024-09-20 10:42:32 +0900

    Add 🌟⭐⭐⭐⭐

--- 백준/그래프/그래프 11403 (경로 찾기) - SILVER 1.java
+++ 백준/그래프/그래프 11403 (경로 찾기) - SILVER 1.java
@@ -1,153 +1,49 @@
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
+import java.util.StringTokenizer;
 import java.util.*;
 
-public class Main {
-	static int N;
-	static int[][] array;
-	static StringBuilder sb;
+public class Solution {
 
-	public static void main(String[] args) throws Exception {
-		// TODO Auto-generated method stub
-		SetData();
-		System.out.println(sb);
-	}
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-	private static void SetData() throws Exception {
-		InputReader in = new InputReader(System.in);
-
-		N = in.nextInt();
-		sb = new StringBuilder();
-		array = new int[N][N];
-
-		for(int i = 0; i < N; i++) {
-			for(int j = 0; j < N; j++) {
-				array[i][j] = in.nextInt();
+		int n = Integer.parseInt(br.readLine());
+		int[][] arr = new int[n][n];
+		List<Integer>[] nodes = new List[n];
+		for(int i = 0; i < n; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			nodes[i] = new ArrayList<>();
+			for(int j = 0; j < n; j++) {
+				arr[i][j] = Integer.parseInt(st.nextToken());
+				if(arr[i][j] == 0) continue;
+				nodes[i].add(j);
 			}
 		}
 
-		for(int k = 0; k < N; k++) {
-			for(int i = 0; i < N; i++) {
-				for(int j = 0; j < N; j++) {
-					// j까지 도달한다면
-					if(array[i][k] + array[k][j] == 2) {
-						array[i][j] = 1;
-					}
+		for(int i = 0; i < n; i++) {
+			Queue<Integer> q = new LinkedList<>();
+			for(int next : nodes[i]) {
+				q.add(next);
+			}
+			while(!q.isEmpty()) {
+				int cur = q.poll();
+
+				for(int next : nodes[cur]) {
+					if(arr[i][next] == 1) continue;
+
+					arr[i][next] = 1;
+					q.add(next);
 				}
 			}
 		}
 
-		for(int i = 0; i < N; i++) {
-			for(int j = 0; j < N; j++) {
-				sb.append(array[i][j] + " ");
+		StringBuilder sb = new StringBuilder();
+		for(int i = 0; i < n; i++) {
+			for(int j = 0; j < n; j++) {
+				sb.append(arr[i][j]).append(" ");
 			}
 			sb.append("\n");
 		}
-	}
-}
-
-class Node {
-	int index;
-	int brotherIndex;
-	int indexJump;
-	int count;
-
-	public Node(int index, int brotherIndex, int indexJump, int count) {
-		this.index = index;
-		this.brotherIndex = brotherIndex;
-		this.indexJump = indexJump+1;
-		this.count = count;
-	}
-}
-
-class InputReader {
-	private final InputStream stream;
-	private final byte[] buf = new byte[8192];
-	private int curChar, snumChars;
-
-	public InputReader(InputStream st) {
-		this.stream = st;
-	}
-
-	public int read() {
-		if (snumChars == -1)
-			throw new InputMismatchException();
-		if (curChar >= snumChars) {
-			curChar = 0;
-			try {
-				snumChars = stream.read(buf);
-			} catch (IOException e) {
-				throw new InputMismatchException();
-			}
-			if (snumChars <= 0)
-				return -1;
-		}
-		return buf[curChar++];
-	}
-
-	public int nextInt() {
-		int c = read();
-		while (isSpaceChar(c)) {
-			c = read();
-		}
-		int sgn = 1;
-		if (c == '-') {
-			sgn = -1;
-			c = read();
-		}
-		int res = 0;
-		do {
-			res *= 10;
-			res += c - '0';
-			c = read();
-		} while (!isSpaceChar(c));
-		return res * sgn;
-	}
-
-	public long nextLong() {
-		int c = read();
-		while (isSpaceChar(c)) {
-			c = read();
-		}
-		int sgn = 1;
-		if (c == '-') {
-			sgn = -1;
-			c = read();
-		}
-		long res = 0;
-		do {
-			res *= 10;
-			res += c - '0';
-			c = read();
-		} while (!isSpaceChar(c));
-		return res * sgn;
-	}
-
-	public int[] nextIntArray(int n) {
-		int a[] = new int[n];
-		for (int i = 0; i < n; i++) {
-			a[i] = nextInt();
-		}
-		return a;
-	}
-
-	public String nextLine() {
-		int c = read();
-		while (isSpaceChar(c))
-			c = read();
-		StringBuilder res = new StringBuilder();
-		do {
-			res.appendCodePoint(c);
-			c = read();
-		} while (!isEndOfLine(c));
-		return res.toString();
-	}
-
-	public boolean isSpaceChar(int c) {
-		return c == ' ' || c == '\n' || c == '\r' || c == '\t' || c == -1;
-	}
-
-	private boolean isEndOfLine(int c) {
-		return c == '\n' || c == '\r' || c == -1;
+		System.out.println(sb);
 	}
 }
