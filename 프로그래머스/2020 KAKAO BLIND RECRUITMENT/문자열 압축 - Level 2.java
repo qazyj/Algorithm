43cdadb3b5239b952f5c0c2bffc18b07fe2805d5
김영진 <qazyj@naver.com>
2024-09-21 23:33:00 +0900

    Add 🌟⭐⭐⭐⭐

--- 프로그래머스/2020 KAKAO BLIND RECRUITMENT/문자열 압축 - Level 2.java
+++ 프로그래머스/2020 KAKAO BLIND RECRUITMENT/문자열 압축 - Level 2.java
@@ -1,48 +1,38 @@
-public class Solution {
-    public int solution(String s) {
-      int answer = s.length();
+class Solution {
+  public int solution(String s) {
+    int length = s.length();
+    int answer = length;
 
-      for (int i = 1; i <= s.length() / 2; i++) {
-        int compLength = compression(s, i).length();
-        answer = Math.min(answer, compLength);
-      }
-
-      return answer;
-    }
-
-    private String compression(String string, int i) {
-
+    for (int i = 1; i <= length/2; i++) {
+      String prev = s.substring(0, i);
       int count = 1;
-      String compression = "";
-      String pattern = "";
+      String result = "";
+      String temp = "";
 
-      for (int j = 0; j <= string.length() + i; j += i) {
-
-        String nowString;
-
-        // 전 문자열과 비교할 현재 문자열
-        if (j >= string.length()) { // 현재 문자열이 없을 때
-          nowString = "";
-        } else if (string.length() < j + i) { // 마지막 현재 문자열일 때
-          nowString = string.substring(j);
+      for (int j = i; j < length; j += i) {
+        if (j + i > s.length()) {
+          temp = s.substring(j);
+          continue;
+        }
+        if (prev.equals(s.substring(j, j + i))) {
+          count++;
         } else {
-          nowString = string.substring(j, j + i);
-        }
-
-        // 1. 전 문자열이랑 똑같은지 비교한다. (맨 처음이면 비교 X)
-        if (j != 0) {
-          if (nowString.equals(pattern)) { // 같으면
-            count++;
-          } else if (count >= 2) { // 다르고 count가 2회 이상
-            compression += count + pattern;
-            count = 1;
-          } else { // 압축 불가능
-            compression += pattern;
+          result += prev;
+          if (count != 1) {
+            result = count + result;
           }
+          prev = s.substring(j, j + i);
+          count = 1;
         }
-        pattern = nowString;
+      }
+      result += prev + temp;
+      if (count != 1) {
+        result = count + result;
       }
 
-      return compression;
+      answer = Math.min(answer, result.length());
     }
+
+    return answer;
+  }
 }
